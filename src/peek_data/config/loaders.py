f0942ab4cc06f0a9d81efaa19c110c583c1af4bd
YooSunyoung <luysunyoung9@gmail.com>
2023-03-09 13:26:16 +0100

    Remove unnecessary string adjustments

--- src/peek_data/config/loaders.py
+++ src/peek_data/config/loaders.py
@@ -6,57 +6,46 @@
 
 import importlib.resources
 from functools import lru_cache, partial
+from os import listdir
+
+import yaml
 
 
-def _read_text(filename):
-    if hasattr(importlib.resources, "files"):
-        # Use new API added in Python 3.9
-        return (
-            importlib.resources.files("peek_data.config.resources")
-            .joinpath(filename)
-            .read_text()
-        )
-    # Old API, deprecated as of Python 3.11
-    return importlib.resources.read_text("peek_data.config.resources", filename)
+def _read_text(path, module="peek_data.config.resources"):
+    return importlib.resources.files(module).joinpath(path).read_text()
 
 
-def _preprocess_yaml(template: str) -> str:
-    import re
+@lru_cache(maxsize=1)
+def _get_number_of_templates():
+    tpl_dir = importlib.resources.files("peek_data.config.resources.templates")
+    return len([f_name for f_name in listdir(tpl_dir) if f_name.endswith(".yaml")])
 
-    # remove comments
-    template = re.sub(r"#(.+)", "", template)
-    # remove empty spaces at the end
-    template = re.sub(r"(\s+)$", "", template)
-    # remove unnecessary line breaks
-    template = re.sub(r"\n(\n+)", "\n", template)
-    return re.sub(r"^\n", "", template)
+
+@lru_cache(maxsize=_get_number_of_templates())
+def _read_template(tpl_name):
+    module = "peek_data.config.resources.templates"
+    return _read_text(tpl_name + ".yaml", module=module)
 
 
 @lru_cache(maxsize=1)
 def load_default_config_yaml() -> str:
     default_config = _read_text("default-setting.yaml")
-    return _preprocess_yaml(default_config)
+    return default_config
 
 
 @lru_cache(maxsize=4)
 def load_user_config_yaml(path: str) -> str:
-    return _preprocess_yaml(_read_text(path))
+    return _read_text(path)
 
 
-@lru_cache(maxsize=5)
-def _load_config_yaml_tpl(name: str) -> str:
-    config_tpl = _read_text(name + ".yaml.template")
-    return _preprocess_yaml(config_tpl)
+def _load_yaml_tpl(name: str) -> str:
+    return yaml.safe_load(_read_template(name))
 
 
-load_config_tpl = partial(_load_config_yaml_tpl, name="config")
-load_data_stream_mapping_tpl = partial(
-    _load_config_yaml_tpl, name="data-stream-mapping"
-)
-load_data_stream_interface_tpl = partial(
-    _load_config_yaml_tpl, name="data-stream-interface"
-)
-load_target_tpl = partial(_load_config_yaml_tpl, name="target")
-load_workflow_tpl = partial(_load_config_yaml_tpl, name="workflow")
-load_kafka_tpl = partial(_load_config_yaml_tpl, name="kafka")
-load_internal_tpl = partial(_load_config_yaml_tpl, name="internal-byte-stream")
+load_config_tpl = partial(_load_yaml_tpl, name="config")
+load_data_stream_mapping_tpl = partial(_load_yaml_tpl, name="data-stream-mapping")
+load_data_stream_interface_tpl = partial(_load_yaml_tpl, name="data-stream-interface")
+load_target_tpl = partial(_load_yaml_tpl, name="target")
+load_workflow_tpl = partial(_load_yaml_tpl, name="workflow")
+load_kafka_tpl = partial(_load_yaml_tpl, name="kafka")
+load_internal_tpl = partial(_load_yaml_tpl, name="internal-byte-stream")
