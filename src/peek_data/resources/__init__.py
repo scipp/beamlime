7a31a2e0edb201007c7e9f2ca7642342c3f88535
YooSunyoung <luysunyoung9@gmail.com>
2023-03-09 18:29:26 +0100

    Reorganize function calls and modules

--- src/peek_data/resources/__init__.py
+++ src/peek_data/resources/__init__.py
@@ -1,3 +1,5 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2023 Scipp contributors (https://github.com/scipp)
-# @author Sunyoung Yoo
+# flake8: noqa F401
+
+from .loaders import find_source, load_default_config, load_yaml, read_source
