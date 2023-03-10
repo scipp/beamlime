9e1c0fd4ee4b04d129698f61ccec0d61a465811d
YooSunyoung <luysunyoung9@gmail.com>
2023-03-10 14:22:54 +0100

    Diagram generating environment in tox.ini

--- docs/diagram.py
+++ docs/diagram.py
@@ -1,61 +1,27 @@
-from diagrams import Diagram, Cluster, Edge
-from diagrams.oci.database import DatabaseService
-from diagrams.oci.storage import ObjectStorage, Buckets
-from diagrams.oci.governance import Audit
-from diagrams.programming.flowchart import Action
-from diagrams.generic.virtualization import Virtualbox
-from diagrams.oci.monitoring import Telemetry
-from diagrams.onprem.queue import Kafka
+from diagrams import Cluster, Diagram, Edge
 from diagrams.custom import Custom
 from diagrams.gcp.operations import Monitoring
+from diagrams.oci.governance import Audit
+from diagrams.oci.monitoring import Telemetry
+from diagrams.oci.storage import ObjectStorage
+from diagrams.onprem.queue import Kafka
 
-
-with Diagram(name="Livesteram Data Reduction System Candidate 1", show=False):
-    
-    with Cluster("Instrument VM"):
-        shared_place = Buckets("shared")
-        ps_control = Monitoring("Process Controller")
-        config = Audit("Congfiguration")
-
-        with Cluster("Data Handling"):
-            wf_control = DatabaseService("Workflow Control")
-            with Cluster("Data Reduction Workflow"):
-                data_reductions = [Custom("Sub Workflow 3", './docs/logo.png'),
-                                Custom("Sub Workflow 2", './docs/logo.png'),
-                               Custom("Sub Workflow 1", './docs/logo.png')]
-
-        with Cluster("Visualization"):
-            web_browser = ObjectStorage('Web Browser')
-            dash_builder = Telemetry("Dashboard Builder")
-            ps_control << web_browser
-
-        kaf_listener = Kafka("Listener")
-        kaf_listener >> wf_control
-        wf_control >> data_reductions 
-        data_reductions >> shared_place
-        shared_place >> dash_builder >> shared_place
-        web_browser << dash_builder
-        
-
-    kaf_producer = Kafka("Producer")
-    kaf_producer << Edge(label='subscribe') << kaf_listener
-
-
-with Diagram(name="Livesteram Data Reduction System Candidate 2", show=False):
-    
+with Diagram(name="Livesteram Data Reduction System Overview", show=False):
     with Cluster("Instrument VM"):
         ps_control = Monitoring("Process Controller \n(backend)")
         config = Audit("Congfiguration")
 
         with Cluster("Visualization"):
-            web_browser = ObjectStorage('Web Browser')
+            web_browser = ObjectStorage("Web Browser")
             dash_builder = Telemetry("Dashboard Builder")
 
             with Cluster("Data Handling"):
                 with Cluster("Data Reduction Workflow"):
-                    data_reductions = [Custom("Sub Workflow 3", './docs/logo.png'),
-                                    Custom("Sub Workflow 2", './docs/logo.png'),
-                                Custom("Sub Workflow 1", './docs/logo.png')]
+                    data_reductions = [
+                        Custom("Sub Workflow 3", "./docs/resources/logo.png"),
+                        Custom("Sub Workflow 2", "./docs/resources/logo.png"),
+                        Custom("Sub Workflow 1", "./docs/resources/logo.png"),
+                    ]
 
             dash_builder >> Edge() << data_reductions
             dash_builder >> web_browser
@@ -64,6 +30,6 @@ with Diagram(name="Livesteram Data Reduction System Candidate 2", show=False):
         kaf_listener >> dash_builder
 
     kaf_producer = Kafka("Producer")
-    kaf_producer << Edge(label='subscribe') << kaf_listener
+    kaf_producer << Edge(label="subscribe") << kaf_listener
     web_browser >> ps_control >> web_browser
     dash_builder << ps_control
