64daf81cbb7c5b612f1abf74b0a7eb18da080d31
YooSunyoung <luysunyoung9@gmail.com>
2023-03-30 17:00:02 +0200

    Add configuration parsing tool with better doc string

---
+++ src/beamlime/config/preset_options.py
@@ -0,0 +1,78 @@
+from enum import Enum
+
+from .tools import find_home
+
+HOME_DIR = find_home.joinpath("." + __name__)  # ~/.beamlime
+DEFAULT_CONFIG_PATH = HOME_DIR.joinpath(
+    "default-config.yaml"
+)  # ~/.beamlime/default-config.yaml
+DEFAULT_LOG_DIR = HOME_DIR.joinpath("log")  # ~/.beamlime/log
+
+
+class NewDataPolicyOptions(Enum):
+    """
+
+    1. ``REPLACE`` - Default
+    Only new data shall be used.
+    New data will replace the previous data, and it does not save the ``previous`` data.
+
+    2. ``SKIP``
+    New data or previous data shall be used.
+    New data will be used only if it is different from the ``previous`` one
+    and it will replace the ``previous``.
+    ``input`` with ``SKIP`` policy, will work like caching with maximum capacity of 1.
+    ``result`` with ``SKIP`` policy, will not update the ``history``
+    but the process to retrieve the ``result`` will still have to be executed.
+
+    3. ``STACK``
+    Sum of all data collected including the new data shall be used.
+    The length of the data shall stay the same, but each value will be updated.
+
+    4. ``AVERAGE``
+    Average of all data collected including the new data shall be used.
+    # TODO: ``AVERAGE`` and ``APPEND`` may cause too much memory consumption
+    so it should be either forbidden or managed.
+    Currently ``AVERAGE`` will not increase the memory consumption in the
+    ``offline.data_reduction.BeamLimeDataReductionApplication``,
+    since the average is calculated like below.
+
+    Given
+    :math:`old = \text{previous average}`,
+    :math:`new = \text{new data from the data stream}`
+    :math:`total = \text{total number of the collected data including new one}`
+
+    , average is calculated like below
+    :math:`average = \frac{(total-1) \times old}{total} + \frac{new}{total}`
+
+    It will stack the error caused by inaccurate division.
+    Increasing length of the data indefinitely and
+    stacking error should not happen so ``AVERAGE`` logic should be updated.
+
+    5. ``APPEND``
+    New data will extend the previous data.
+    The length of the data may increase, but each value will stay the same.
+    # TODO: We might have to forbid ``APPEND`` at all in most cases
+    in live data reduction.
+
+    """
+
+    REPLACE = "REPLACE"
+    SKIP = "SKIP"
+    STACK = "STACK"
+    AVERAGE = "AVERAGE"
+    APPEND = "APPEND"
+
+
+class CommunicationChannelOptions(Enum):
+    """
+    1. ``KAFKA``
+    #TODO: Not implemented yet.
+
+    2. ``QUEUE``
+    Use ``queue.QUEUE`` as a communication interface between two applications.
+    #TODO: Currently, async is showing unexpected behaviour on the jupyter notebook.
+    We might want to use multiprocess instead of async.
+    """
+
+    KAFKA = "KAFKA"
+    QUEUE = "QUEUE"
--- src/beamlime/config/tools.py
+++ src/beamlime/config/tools.py
@@ -1,7 +1,11 @@
-from typing import Iterable, List, Union
+# ``beamlime.config.tools`` should only contain general helper functions
+# that are repeatedly used by various ``beamlime`` package for configuration parsing.
+# Therefore it should not have any dependencies on other modules/classes/functions.
+
+from typing import Any, AnyStr, Iterable, List, Union
 
 
-def nested_data_get(nested_obj: Iterable, *indices: Union[List, str, int]):
+def nested_data_get(nested_obj: Iterable, *indices: Union[List, str, int]) -> Any:
     """
     Helper function to retrieve an item from a nested dictionary or lists.
     If there is no indices give, it will do nothing and
@@ -33,7 +37,7 @@ def nested_data_get(nested_obj: Iterable, *indices: Union[List, str, int]):
 
     """
 
-    def _nested_data_get(nested_obj: Iterable, *indices):
+    def _nested_data_get(nested_obj: Iterable, *indices) -> Any:
         """
         Inner function of ``nested_data_get`` to avoid repeated argument wrapping.
         """
@@ -61,14 +65,92 @@ def nested_data_get(nested_obj: Iterable, *indices: Union[List, str, int]):
     return _nested_data_get(nested_obj, *indices)
 
 
+class DictionaryItemOverwrittenWarning(Warning):
+    """Overwriting dictionary item might be unexpected."""
+
+
+class DictionaryItemOverwrittenError(Exception):
+    """Overwriting dictionary item is not expected at all."""
+
+
 def list_to_dict(
-    items: list,
-    key_field: Union[str, int] = "name",
-    value_field: Union[List, str, int] = None,
+    items: List,
+    key_field: Union[AnyStr, int] = "name",
+    value_field: Union[AnyStr, int] = None,
+    allow_overwriting: bool = False,
 ) -> dict:
+    """
+    Converts a list to a dictionary based on the ``key_field`` and ``value_field``.
+    From the converted dictionary,
+    each ``item`` will be accessible by it's own member, which is ``item[key_field]``.
+
+    If ``value_field`` is not defined, it will keep all fields of each item, otherwise
+    each item will only keep ``item[value_field]`` in the converted dictionary.
+
+    This helper was needed to prevent unexpected overwriting in the configuration file.
+    See https://github.com/scipp/beamlime/discussions/33 for more details.
+
+    You can also give a list of indices as a ``key_field`` but not recommended.
+
+    Examples
+    --------
+    ```
+    >>> items = [{'name': 'lime0', 'price': 1}, {'name': 'lime1', 'price': 2}]
+    >>> dict_items = list_to_dict(items, key_field='name')
+    >>> dict_items
+    {
+        'lime0': {'name': 'lime0', 'price': 1},
+        'lime1': {'name': 'lime1', 'price': 2}
+    }
+
+    >>> smaller_dict_items = list_to_dict(items, key_field='name', value_field='price')
+    >>> smaller_dict_items
+    {
+        'lime0': 1,
+        'lime1': 2
+    }
+    ```
+
+    Raises
+    ------
+    DictionaryItemOverwrittenWarning
+      If ``allow_overwriting`` == True and any values of ``key_field`` are not unique.
+
+    DictionaryItemOverwrittenError
+      If ``allow_overwriting`` == False and any values of ``key_field`` are not unique.
+    """
     if value_field is None:
-        return {item[key_field]: item for item in items}
-    elif isinstance(value_field, List):
-        {item[key_field]: nested_data_get(item, *value_field) for item in items}
+        converted_dict = {item[key_field]: item for item in items}
     else:
-        return {item[key_field]: item[value_field] for item in items}
+        converted_dict = {item[key_field]: item[value_field] for item in items}
+
+    if len(converted_dict) != len(items):  # If any items are overwritten.
+        message = f"'{key_field}' contains non-unique values. "
+        if allow_overwriting:
+            raise DictionaryItemOverwrittenWarning(message)
+        else:
+            hint = "If it is expected you may set the `allow_overwriting` as `True`."
+            raise DictionaryItemOverwrittenError(message, hint)
+
+    return converted_dict
+
+
+def find_home(env_var_key: str = "BEAMLIME_HOME") -> str:
+    """
+    Returns the home directory of the ``beamlime`` applications,
+    where it saves logs and temporary data.
+    Setting an environment variable will overwrite the default home directory,
+    which is ``~/.beamlime``.
+
+    Example
+    -------
+    ```
+    export BEAMLIME_HOME='another/directory/you/want/to/be/home'
+    ```
+    """
+    import os
+    from pathlib import Path
+
+    from beamlime import __name__
+
+    return os.environ.get(env_var_key, Path.home().joinpath("." + __name__))
