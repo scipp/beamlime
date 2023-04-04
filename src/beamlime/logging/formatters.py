a43247a9f666c8b555ce6ae10e727c5c574326c4
YooSunyoung <luysunyoung9@gmail.com>
2023-04-04 19:00:18 +0200

    Wrap a list of header-columns and into LogHeader class

--- src/beamlime/logging/formatters.py
+++ src/beamlime/logging/formatters.py
@@ -1,15 +1,16 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2023 Scipp contributors (https://github.com/scipp)
 
+from collections import OrderedDict
 from functools import partial
 from logging import Formatter
-from typing import Any, List, Literal, Mapping, TypeAlias
+from typing import Any, Literal, Mapping, TypeAlias, overload
 
 _FormatStyle: TypeAlias = Literal["{"]
 _SepStyle: TypeAlias = Literal["|", ","]
 
 
-class LogHeader:
+class LogColumn:
     def __init__(
         self,
         variable_name: str,
@@ -43,45 +44,82 @@ class LogHeader:
         )
 
 
-def retrieve_headers_from_fmt(fmt: str) -> List[LogHeader]:
-    def wrap_header(fmt_piece: str) -> LogHeader:
-        if ":" in fmt_piece:
-            variable_name, min_length_str = fmt_piece.split(":")
-            return LogHeader(variable_name, min_length=int(min_length_str))
-        else:
-            LogHeader(fmt_piece)
+class LogHeader(OrderedDict):
+    @overload
+    def __init__(self, *fmt: str, padding: tuple, sep: _SepStyle):
+        ...
 
-    chunks = fmt.split("{")
-    fmt_pieces = [chunk.split("}")[0] for chunk in chunks]
-    return [wrap_header(fmt_piece) for fmt_piece in fmt_pieces]
+    @overload
+    def __init__(self, *columns: LogColumn, padding: tuple, sep: _SepStyle):
+        ...
+
+    def __init__(
+        self, *args: str | LogColumn, padding: tuple = (1, 1), sep: _SepStyle = "|"
+    ):
+        self.padding = padding
+        self.sep = sep
+        if len(args) > 1 and isinstance(args[0], LogColumn):
+            columns = args
+        elif len(args) > 1 and isinstance(args[0], str):
+            fmt = "".join(args)
+
+            def wrap_header(fmt_piece: str) -> LogColumn:
+                if ":" in fmt_piece:
+                    variable_name, min_length_str = fmt_piece.split(":")
+                    return LogColumn(variable_name, min_length=int(min_length_str))
+                else:
+                    LogColumn(fmt_piece)
+
+            chunks = fmt.split("{")
+            fmt_pieces = [chunk.split("}")[0] for chunk in chunks]
+            columns = [wrap_header(fmt_piece) for fmt_piece in fmt_pieces]
+
+        for column in columns:
+            self.__setitem__(column.variable_name, column)
+
+    def to_fmt(self):
+        columns = list(self.values())
+        if len(columns) == 1:
+            return columns[0].formatter
+
+        seps = [
+            " " * self.padding[0] + self.sep + " " * self.padding[1]
+            if column.visible and n_column.visible
+            else ""
+            for column, n_column in zip(columns[:-1], columns[1:])
+        ]
+        return "".join(
+            [column.formatter + sep for column, sep in zip(columns, seps + [""])]
+        )
+
+    def __getitem__(self, key: str) -> LogColumn:
+        return super().__getitem__(key)
+
+    def __setitem__(self, key: str, item: LogColumn) -> None:
+        key = item.variable_name
+        return super().__setitem__(key, item)
+
+    def format(self) -> str:
+        return self.to_fmt().format(
+            **{column.variable_name: column.title for column in self.values()}
+        )
 
 
-def headers_to_fmt(*headers: LogHeader, padding: tuple = (1, 1), sep: _SepStyle = "|"):
-    if len(headers) == 1:
-        return headers[0].formatter
+DEFAULT_HEADERS = LogHeader(
+    LogColumn("asctime", title="TIME", min_length=23),
+    LogColumn("app_name", title="APPLICATION", min_length=15),
+    LogColumn("levelname", title="LEVEL", min_length=8),
+    LogColumn("message"),
+    padding=(1, 1),
+    sep="|",
+)
 
-    seps = [
-        " " * padding[0] + sep + " " * padding[1]
-        if header.visible and n_header.visible
-        else ""
-        for header, n_header in zip(headers[:-1], headers[1:])
-    ]
-    return "".join(
-        [header.formatter + sep for header, sep in zip(headers, seps + [""])]
-    )
-
-
-DEFAULT_HEADERS = [
-    LogHeader("asctime", title="TIME", min_length=23),
-    LogHeader("app_name", title="APPLICATION", min_length=15),
-    LogHeader("levelname", title="LEVEL", min_length=8),
-    LogHeader("message"),
-]
-
-DEFAULT_COLOR_HEADERS = (
-    [LogHeader("ansi_color", title="", visible=False)]
-    + DEFAULT_HEADERS
-    + [LogHeader("reset_color", title="", visible=False)]
+DEFAULT_COLOR_HEADERS = LogHeader(
+    LogColumn("ansi_color", title="", visible=False),
+    *DEFAULT_HEADERS.values(),
+    LogColumn("reset_color", title="", visible=False),
+    padding=(1, 1),
+    sep="|",
 )
 
 
@@ -95,17 +133,14 @@ class _HeaderFormatter(Formatter):
         *,
         defaults: Mapping[str, Any] | None = None,
         header_sep: _SepStyle = "|",
-        headers: List[LogHeader] = DEFAULT_HEADERS,
+        headers: LogHeader = DEFAULT_HEADERS,
     ) -> None:
         if fmt is None and headers is not None:
-            fmt = headers_to_fmt(*headers, padding=(1, 1), sep=header_sep)
-
+            fmt = headers.to_fmt()
         elif fmt is not None:
-            headers = retrieve_headers_from_fmt(fmt)
+            headers = LogHeader(fmt, padding=(1, 1), sep=header_sep)
 
-        self._header = fmt.format(
-            **{header.variable_name: header.title for header in headers}
-        )
+        self._header = headers.format()
         super().__init__(fmt, datefmt, style, validate, defaults=defaults)
 
     @property
