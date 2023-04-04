723f99cba4f24aec1b36b0051aa2798a9611810f
YooSunyoung <luysunyoung9@gmail.com>
2023-04-04 17:42:56 +0200

    Remove LogRecordFactory setting from __init__ to avoid unexpected behaviour

--- src/beamlime/logging/__init__.py
+++ src/beamlime/logging/__init__.py
@@ -2,9 +2,5 @@
 # Copyright (c) 2023 Scipp contributors (https://github.com/scipp)
 # flake8: noqa F401
 
-import logging
-
 from .logging import get_logger, initialize_file_handler
 from .records import BeamlimeColorLogRecord
-
-logging.setLogRecordFactory(BeamlimeColorLogRecord)
