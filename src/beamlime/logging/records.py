250980fc5a855318c4f3b19d8a59771f47212a16
YooSunyoung <luysunyoung9@gmail.com>
2023-04-05 15:54:30 +0200

    Remove hard-coded hex code and replace it with symbol

--- src/beamlime/logging/records.py
+++ src/beamlime/logging/records.py
@@ -28,17 +28,17 @@ class BeamlimeLogRecord(LogRecord):
         if isinstance(msg, dict):
             if "app_name" in msg:
                 self.app_name = msg.get("app_name")
-                self.msg = msg.get("msg")
+                _msg = msg.get("msg")
             else:
-                self.msg = str(msg)
+                _msg = str(msg)
                 self.exc_info = Warning(
                     "Unexpected form of message for BeamlimeLogRecord."
                 )
         else:
             self.app_name = ""
-            self.msg = msg
+            _msg = msg
         super().__init__(
-            name, level, pathname, lineno, self.msg, args, exc_info, func, sinfo
+            name, level, pathname, lineno, _msg, args, exc_info, func, sinfo
         )
 
     @final
@@ -71,8 +71,10 @@ class BeamlimeColorLogRecord(BeamlimeLogRecord):
         func: str | None = None,
         sinfo: str | None = None,
     ) -> None:
+        from colorama import Style
+
         self.ansi_color = ""
-        self.reset_color = "\033[0m"
+        self.reset_color = Style.RESET_ALL
         super().__init__(
             name, level, pathname, lineno, msg, args, exc_info, func, sinfo
         )
