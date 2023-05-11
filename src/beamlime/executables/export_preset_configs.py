5368392eb0625038a556a2ac3cd42575b4010616
YooSunyoung <luysunyoung9@gmail.com>
2023-05-11 16:07:02 +0200

    Update static preset configuration files to have kafka communication channels

--- src/beamlime/executables/export_preset_configs.py
+++ src/beamlime/executables/export_preset_configs.py
@@ -1,16 +1,15 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2023 Scipp contributors (https://github.com/scipp)
 
-from ..resources.exporters import export_default_yaml
 
 if __name__ == "__main__":
     import argparse
 
+    from ..resources.exporters import export_default_yaml, export_fake_2d
+
     parser = argparse.ArgumentParser()
     parser.add_argument("--directory", default="./")
-    parser.add_argument("--name", default="default-setting.yaml")
     parser.add_argument("--overwrite", action=argparse.BooleanOptionalAction)
     args = parser.parse_args()
-    export_default_yaml(
-        directory=args.directory, filename=args.name, overwrite=args.overwrite
-    )
+    export_fake_2d(directory=args.directory, overwrite=args.overwrite)
+    export_default_yaml(directory=args.directory, overwrite=args.overwrite)
