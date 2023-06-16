8df0e6ce836ff71f0647441e0b844268d48e3bfa
YooSunyoung <luysunyoung9@gmail.com>
2023-06-16 10:43:48 +0200

    Wipe out source code for easier review on next bigger updates

--- src/beamlime/applications/__init__.py
+++
@@ -1,6 +0,0 @@
-# SPDX-License-Identifier: BSD-3-Clause
-# Copyright (c) 2023 Scipp contributors (https://github.com/scipp)
-# flake8: noqa F401
-
-from .groups import MAX_INSTANCE_NUMBER, BeamlimeApplicationInstanceGroup
-from .interfaces import BeamlimeApplicationInterface, BeamlimeDataReductionInterface
