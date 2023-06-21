fcdd392d3d62ded4e51bbcf81eb3e3def7d56831
YooSunyoung <luysunyoung9@gmail.com>
2023-06-22 01:14:21 +0200

    Dependency Injection done by factory, instead of binder/provider/container

---
+++ src/beamlime/constructors/injection.py
@@ -0,0 +1,173 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2023 Scipp contributors (https://github.com/scipp)
+
+from __future__ import annotations
+
+from typing import Any, Dict, Type
+
+from .base import (
+    FactoryBase,
+    MismatchingProductTypeError,
+    ProviderExistsError,
+    ProviderNotFoundError,
+    _Product,
+)
+from .inspectors import DependencySpec, ProductType, UnknownType
+from .providers import Constructor, Provider
+
+
+class RegistrationInterface(FactoryBase):
+    """Provider registration interfaces."""
+
+    def register(self, product_type: ProductType, provider_call: Constructor) -> None:
+        """
+        Register a provider function ``provider_call`` of ``product_type``
+        into the ``self._providers`` dictionary.
+
+        If ``product_type`` is ``UnknownType``,
+        it will retrieve the product type from the provider call
+        and use it as an index.
+
+        ``self.provider`` decorator from ``beamlime.constructors`` module
+        can also register a function or a class as a provider.
+
+        See ``_validate_new_provider`` for new provider validation.
+
+        """
+        new_provider_call = Provider(provider_call)
+        if product_type == UnknownType:
+            _product_type = new_provider_call.product_spec.product_type
+        else:
+            _product_type = product_type
+
+        self._validate_new_provider(_product_type, new_provider_call)
+        self._providers[_product_type] = new_provider_call
+
+    def _validate_new_provider(
+        self, product_type: ProductType, new_provider: Provider
+    ) -> None:
+        """
+        Raises
+        ------
+        ProviderExistsError:
+            If there is an existing provider of the ``product_type``.
+            However, if the full name of the ``new_provider_call``
+            is same as the existing one, it will register
+            the new callable object as the provider.
+
+        MismatchingProductTypeError:
+            If the return type of the ``provider_call`` is
+            incompatible with ``product_type``.
+            The return type of the ``provider_call`` should be a subclass of
+            ``product_type``or the same object as ``product_type``.
+            See ``ProviderCall.is_supprted_type``.
+        """
+        if product_type in self._providers:
+            existing_provider = self._providers[product_type]
+            if existing_provider != new_provider:
+                raise ProviderExistsError(
+                    f"Can not register ``{new_provider.call_name}``."
+                    f"Provider for ``{product_type.__name__}`` "
+                    f"already exist. ``{existing_provider.call_name}``"
+                )
+        if not new_provider.can_provide(product_type):
+            raise MismatchingProductTypeError(
+                f"{product_type} can not be provided by " f"{new_provider.constructor}"
+            )
+
+    def provider(self, callable_obj: Constructor):
+        """
+        Register the decorated provider function into this factory.
+        """
+        from .inspectors import UnknownType
+
+        self.register(UnknownType, callable_obj)
+        return callable_obj
+
+
+class Empty:
+    ...
+
+
+class InjectionInterface(RegistrationInterface):
+    """Dependency injection interfaces."""
+
+    def build_arg_dependency(self, arg_spec: DependencySpec):
+        try:
+            return self.assemble(arg_spec.product_type)
+        except ProviderNotFoundError as err:
+            from inspect import Signature
+
+            from .inspectors import UnknownType
+
+            if (arg_spec.default_product != Signature.empty) or (
+                arg_spec.product_type == UnknownType
+            ):
+                return Empty
+            raise err
+
+    def build_arguments(self, provider: Provider):
+        return {
+            arg_name: _product
+            for arg_name, arg_spec in provider.arg_dep_specs.items()
+            if (_product := self.build_arg_dependency(arg_spec)) != Empty
+        }
+
+    def build_attr_dependency(self, dep_type: ProductType):
+        try:
+            return self.assemble(dep_type)
+        except ProviderNotFoundError:
+            return Empty
+
+    def inject_dependencies(self, _obj: _Product) -> _Product:
+        """
+        Check if the ``incomplete_obj`` has attribute dependencies to be filled.
+        If a provider is not found but the ``incomplete_obj`` already has the attribute,
+        it skips the attribute injection.
+
+        Raises
+        ------
+            ProviderNotFoundError:
+                If a provider is not found for the attribute and
+                the ``incomplete_obj`` still doesn't have the attribute.
+
+        Attribute annotations are retrieved by typing.get_type_hints.
+        """
+        from typing import get_type_hints
+
+        _attrs: Dict[str, Any] = {
+            _attr_name: self.build_attr_dependency(attr_type)
+            for _attr_name, attr_type in get_type_hints(_obj.__class__).items()
+        }
+
+        def _is_missing(attr, attr_name: str):
+            return attr is Empty and not hasattr(_obj, attr_name)
+
+        if any(
+            missing_attrs := [
+                _attr_name
+                for _attr_name, _attr in _attrs.items()
+                if _is_missing(_attr, _attr_name)
+            ]
+        ):
+            raise ProviderNotFoundError(
+                f"Provider for the attribute(s) {missing_attrs} not found "
+                f"during the dependency injection to the {_obj.__class__} instance."
+            )
+
+        for _attr_name, _attr in _attrs.items():
+            if _attr is Empty:
+                ...  # Skip injecting dependency.
+                # The other case where attr_subprovider
+                # is not found but the ``_obj`` does not
+                # have the attribute is filtered
+                # by checking the missing attributes above.
+            else:
+                setattr(_obj, _attr_name, _attr)
+        return _obj
+
+    def assemble(self, product_type: Type[_Product]) -> _Product:
+        _provider = self.find_provider(product_type)
+        kwargs = {**self.build_arguments(_provider), **_provider.keywords}
+        _obj = _provider.constructor(*_provider.args, **kwargs)
+        return self.inject_dependencies(_obj)
--- tests/constructors/provider_decorator_test.py
+++ tests/constructors/provider_decorator_test.py
@@ -2,58 +2,42 @@
 # Copyright (c) 2023 Scipp contributors (https://github.com/scipp)
 import pytest
 
-from beamlime.constructors import GlobalBinder, local_binder, provider
-
-from .preset_binder import GoodTelling, TestBinder, give_a_good_telling
-
-
-def integer_provider() -> int:
-    return 10
-
-
-def test_global_provider_decorator():
-    original_call = provider(integer_provider)  # Call the decorator directly.
-    assert original_call is integer_provider
-    assert GlobalBinder()[int].constructor is integer_provider
-    assert GlobalBinder()[int]() == integer_provider()
-    GlobalBinder().clear()
-
-
-def test_provider_function_call():
-    assert TestBinder[GoodTelling].constructor == give_a_good_telling
-    assert TestBinder[GoodTelling]() == give_a_good_telling()
+from .preset_factory import test_factory
 
 
 def test_provider_incomplete_class_raises():
-    with local_binder():
+    with test_factory.local_factory():
         from beamlime.constructors import ProviderNotFoundError
 
-        from .preset_binder import Parent
+        from .preset_factory import Parent
 
         with pytest.raises(ProviderNotFoundError):
-            TestBinder[Parent]()
+            test_factory[Parent]
 
 
 def test_provider_member_class_raises():
-    with pytest.raises(NotImplementedError):
+    with test_factory.local_factory() as factory:
+        with pytest.raises(NotImplementedError):
 
-        @provider
-        class _:
-            ...
-
-
-def test_provider_member_function_raises():
-    with pytest.raises(NotImplementedError):
-
-        class _:
-            @provider
-            def __(self):
+            @factory.provider
+            class _:
                 ...
 
 
-def test_provider_local_function_raises():
-    with pytest.raises(NotImplementedError):
+def test_provider_member_function_raises():
+    with test_factory.local_factory() as factory:
+        with pytest.raises(NotImplementedError):
 
-        @provider
-        def _() -> None:
-            return None
+            class _:
+                @factory.provider
+                def __(self):
+                    ...
+
+
+def test_provider_local_function_raises():
+    with test_factory.local_factory() as factory:
+        with pytest.raises(NotImplementedError):
+
+            @factory.provider
+            def _() -> None:
+                return None
