5742455fc1682eaec8c762ad5b81c3acf1e12afb
YooSunyoung <luysunyoung9@gmail.com>
2023-07-12 01:01:39 +0200

    Add type hints

--- src/beamlime/core/schedulers.py
+++ src/beamlime/core/schedulers.py
@@ -2,27 +2,31 @@
 # Copyright (c) 2023 Scipp contributors (https://github.com/scipp)
 # async-related tools
 
-from typing import Callable, Type
+from typing import Any, Awaitable, Callable, Type, TypeVar
 
 
 class MaxTrialsReached(Exception):
     ...
 
 
+T = TypeVar("T")
+WrappedAsyncCallable = Callable[..., Awaitable[T]]
+
+
 def async_retry(
-    *exceptions: Type, max_trials: int = 1, interval: float = 0
-) -> Callable:
+    *exceptions: Type[Exception], max_trials: int = 1, interval: float = 0
+) -> Callable[..., WrappedAsyncCallable[T]]:
     """
     Retry calling an async method under expected exceptions.
 
     """
 
-    def inner_decorator(func: Callable) -> Callable:
+    def inner_decorator(func: WrappedAsyncCallable[T]) -> WrappedAsyncCallable[T]:
         import asyncio
         from functools import wraps
 
         @wraps(func)
-        async def wrapper(*args, **kwargs):
+        async def wrapper(*args: Any, **kwargs: Any) -> T:
             for _ in range(max_trials - 1):
                 try:
                     return await func(*args, **kwargs)
@@ -36,18 +40,23 @@ def async_retry(
     return inner_decorator
 
 
-def retry(*exceptions: Type, max_trials: int = 1, interval: float = 0) -> Callable:
+WrappedCallable = Callable[..., T]
+
+
+def retry(
+    *exceptions: Type[Exception], max_trials: int = 1, interval: float = 0
+) -> Callable[..., WrappedCallable[T]]:
     """
     Retry calling a method under expected exceptions.
 
     """
 
-    def inner_decorator(func: Callable) -> Callable:
+    def inner_decorator(func: WrappedCallable[T]) -> WrappedCallable[T]:
         import time
         from functools import wraps
 
         @wraps(func)
-        def wrapper(*args, **kwargs):
+        def wrapper(*args: Any, **kwargs: Any) -> T:
             for _ in range(max_trials - 1):
                 try:
                     return func(*args, **kwargs)
