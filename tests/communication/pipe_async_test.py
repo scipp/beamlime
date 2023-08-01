485769449222c5aa9a4556ff53b5d3748aa268f0
YooSunyoung <luysunyoung9@gmail.com>
2023-08-01 13:54:04 +0200

    Use pytest asyncio mark instead of asyncio.run

--- tests/communication/pipe_async_test.py
+++ tests/communication/pipe_async_test.py
@@ -2,6 +2,8 @@
 # Copyright (c) 2023 Scipp contributors (https://github.com/scipp)
 import asyncio
 
+import pytest
+
 from beamlime.communication.pipes import Pipe
 
 
@@ -10,7 +12,8 @@ async def run_workers(*workers):
     return await asyncio.gather(*tasks)
 
 
-def test_pipe_single_reader_writer_async():
+@pytest.mark.asyncio
+async def test_pipe_single_reader_writer_async():
     pipe = Pipe()
 
     async def reader() -> list:
@@ -26,12 +29,12 @@ def test_pipe_single_reader_writer_async():
             pipe.write(i)
             await asyncio.sleep(0.01)
 
-    workers_coroutine = run_workers(writer(), reader())
-    _, result = asyncio.run(workers_coroutine)
+    _, result = await run_workers(writer(), reader())
     assert result == list(range(5))
 
 
-def test_pipe_multiple_readers_async():
+@pytest.mark.asyncio
+async def test_pipe_multiple_readers_async():
     pipe = Pipe(*list(range(10)))
 
     async def reader(delay) -> list:
@@ -43,7 +46,6 @@ def test_pipe_multiple_readers_async():
             await asyncio.sleep(0.01)
         return result
 
-    workers_coroutine = run_workers(reader(0), reader(0.005))
-    reader1_result, reader2_result = asyncio.run(workers_coroutine)
+    reader1_result, reader2_result = await run_workers(reader(0), reader(0.005))
     assert reader1_result == [i * 2 for i in range(5)]
     assert reader2_result == [i * 2 + 1 for i in range(5)]
