5664ecb4ac71037c1f1001d0d97990eb5faa1941
YooSunyoung <luysunyoung9@gmail.com>
2023-10-12 10:54:16 +0200

    Update time coordinate to provide realistic time offset zero

--- tests/prototypes/random_data_providers.py
+++ tests/prototypes/random_data_providers.py
@@ -2,7 +2,7 @@
 # Copyright (c) 2023 Scipp contributors (https://github.com/scipp)
 from __future__ import annotations
 
-from typing import Any, Generator, List, NewType
+from typing import Any, Generator, List, NewType, Optional
 
 import scipp as sc
 from numpy.random import Generator as RNG
@@ -14,6 +14,7 @@ from .parameters import EventRate, FrameRate, NumFrames, NumPixels, RandomSeed
 
 # Derived Configuration
 EventFrameRate = NewType("EventFrameRate", int)  # [events/frame]
+ReferenceTimeZero = NewType("ReferenceTimeZero", int)  # [ns]
 
 # Generated Data
 DetectorCounts = NewType("DetectorCounts", sc.Variable)
@@ -35,14 +36,28 @@ def calculate_event_per_frame(
     return EventFrameRate(int(event_rate / frame_rate))
 
 
-def provide_time_coords(rng: RNG, ef_rate: EventFrameRate) -> TimeCoords:
-    dummy_zeros = [zr for zr in range(int(13620492e11), int(13620492e11) + ef_rate)]
-    et_zero = sc.datetimes(dims=["event"], values=dummy_zeros, unit='ns')
+def provide_time_coords(
+    rng: RNG,
+    ef_rate: EventFrameRate,
+    ref_time: Optional[ReferenceTimeZero] = None,
+) -> TimeCoords:
+    ref_time = ref_time or ReferenceTimeZero(13620492**11)
+    et_zero = sc.datetimes(dims=["event"], values=[ref_time] * ef_rate, unit='ns')
     et_offset = sc.array(
         dims=["event"], values=rng.random((ef_rate,)) * 800 + 200, unit='ns'
     )
 
     return TimeCoords({"event_time_zero": et_zero, "event_time_offset": et_offset})
+
+
+def provide_time_coords_generator(
+    rng: RNG, ef_rate: EventFrameRate, num_frames: NumFrames, frame_rate: FrameRate
+) -> Generator[TimeCoords, Any, Any]:
+    for i_frame in range(num_frames):
+        ref_time = ReferenceTimeZero(
+            int(13620492e11 + (i_frame / frame_rate) * 10**9)
+        )
+        yield provide_time_coords(rng, ef_rate, ref_time)
 
 
 def provide_dummy_counts(ef_rate: EventFrameRate) -> DetectorCounts:
@@ -64,10 +79,10 @@ def provide_random_pixel_id_generator(
 
 def provide_random_event_generator(
     pixel_id_generator: Generator[RandomPixelId, Any, Any],
-    time_coords: TimeCoords,
+    time_coords_generator: Generator[TimeCoords, Any, Any],
     data: DetectorCounts,
 ) -> Generator[RandomEvent, Any, Any]:
-    for pixel_id in pixel_id_generator:
+    for pixel_id, time_coords in zip(pixel_id_generator, time_coords_generator):
         yield RandomEvent(
             sc.DataArray(data=data, coords={"pixel_id": pixel_id, **time_coords})
         )
@@ -88,6 +103,7 @@ def provide_random_events(
 random_data_providers = ProviderGroup(
     provide_rng,
     provide_random_pixel_id_generator,
+    provide_time_coords_generator,
     provide_random_event_generator,
     provide_random_events,
     SingletonProvider(calculate_event_per_frame),
@@ -101,12 +117,14 @@ def dump_random_dummy_events() -> RandomEvents:
     event_frame_rate = EventFrameRate(10_000)
     num_frames = NumFrames(10)
     rng = provide_rng(RandomSeed(123))
-    time_coords = provide_time_coords(rng, event_frame_rate)
+    time_coords_generator = provide_time_coords_generator(
+        rng, event_frame_rate, num_frames
+    )
     data = provide_dummy_counts(event_frame_rate)
     pixel_id_generator = provide_random_pixel_id_generator(
         rng, event_frame_rate, num_pixels, num_frames
     )
     random_event_generator = provide_random_event_generator(
-        pixel_id_generator, time_coords, data
+        pixel_id_generator, time_coords_generator, data
     )
     return provide_random_events(random_event_generator)
