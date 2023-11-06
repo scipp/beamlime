e16263f45c0171c38bba399c6e8bf83fd3db31c0
YooSunyoung <luysunyoung9@gmail.com>
2023-11-06 10:26:16 +0100

    Use benchmark session, runner helper for prototype testing.

--- tests/prototypes/prototype_test.py
+++ tests/prototypes/prototype_test.py
@@ -1,18 +1,13 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2023 Scipp contributors (https://github.com/scipp)
-from typing import Generator
+from typing import Any, Generator
 
 import pytest
 
 from beamlime.constructors import Factory
 
-from .prototype_mini import (
-    BaseApp,
-    StopWatch,
-    TargetCounts,
-    VisualizationDaemon,
-    run_prototype,
-)
+from ..benchmarks.runner import BenchmarkSession
+from .parameters import PrototypeParameters
 
 
 @pytest.fixture
@@ -24,40 +19,58 @@ def mini_factory() -> Generator[Factory, None, None]:
 
 @pytest.fixture
 def kafka_factory(kafka_test: bool) -> Generator[Factory, None, None]:
-    from .prototype_kafka import KafkaTopicDeleted, kafka_prototype_factory
+    from .prototype_kafka import kafka_prototype_factory
 
     assert kafka_test
-    kafka = kafka_prototype_factory()
-    yield kafka
-    assert kafka[KafkaTopicDeleted]
+    yield kafka_prototype_factory()
 
 
-def prototype_test_helper(prototype_factory: Factory, reference_app_tp: type[BaseApp]):
-    from .parameters import ChunkSize, EventRate, NumFrames, NumPixels
+def prototype_test_helper(prototype_factory: Factory):
+    from .prototype_mini import PrototypeBenchmarkRecipe, PrototypeRunner
 
-    # No laps recorded.
-    assert len(prototype_factory[StopWatch].lap_counts) == 0
+    prototype_runner = PrototypeRunner()
 
-    num_frames = 140
-    chunk_size = 28
-    run_prototype(
-        prototype_factory=prototype_factory,
-        parameters={
-            EventRate: 10**4,
-            NumPixels: 10**4,
-            NumFrames: num_frames,
-            ChunkSize: chunk_size,
-        },
-    )
-    stop_watch = prototype_factory[StopWatch]
-    reference_app_name = prototype_factory[reference_app_tp].app_name
-    assert prototype_factory[TargetCounts] == int(num_frames / chunk_size)
-    assert stop_watch.lap_counts[reference_app_name] == prototype_factory[TargetCounts]
+    recipe = PrototypeBenchmarkRecipe(params=PrototypeParameters())
+    prototype_runner(prototype_factory.providers, recipe)
 
 
 def test_mini_prototype(mini_factory: Factory):
-    prototype_test_helper(mini_factory, VisualizationDaemon)
+    prototype_test_helper(mini_factory)
 
 
 def test_kafka_prototype(kafka_factory: Factory):
-    prototype_test_helper(kafka_factory, VisualizationDaemon)
+    prototype_test_helper(kafka_factory)
+
+
+@pytest.fixture(scope='session')
+def prototype_benchmark(benchmark_test: bool) -> Generator[BenchmarkSession, Any, Any]:
+    from ..benchmarks.runner import BenchmarkRunner, create_benchmark_runner_factory
+    from .prototype_mini import PrototypeRunner
+
+    assert benchmark_test
+
+    benchmark_factory = create_benchmark_runner_factory()
+    with benchmark_factory.temporary_provider(BenchmarkRunner, PrototypeRunner):
+        benchmark_session = benchmark_factory[BenchmarkSession]
+        yield benchmark_session
+
+        benchmark_session.save()
+
+
+def test_mini_prototype_benchmark(prototype_benchmark: BenchmarkSession):
+    from .prototype_mini import (
+        BenchmarkTargetName,
+        PrototypeBenchmarkRecipe,
+        mini_prototype_factory,
+    )
+
+    recipe = PrototypeBenchmarkRecipe(
+        params=PrototypeParameters(), optional_parameters={'test-run': True}
+    )
+
+    with prototype_benchmark.configure(iterations=3):
+        prototype_benchmark.run(
+            mini_prototype_factory().providers,
+            recipe,
+            BenchmarkTargetName('mini_prototype'),
+        )
