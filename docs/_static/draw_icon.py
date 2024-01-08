16a2821bcb2917a0dc6b620f9ce476f9520d1096
YooSunyoung <luysunyoung9@gmail.com>
2024-01-08 11:39:34 +0100

    Add documentation image materials.

--- docs/_static/draw_icon.py
+++ docs/_static/draw_icon.py
@@ -1,55 +1,9 @@
-# This script for drawing logo is one-time used, but it is kept here for reference.
-# To run this script, you may need to install
-# a python package ``drawsvg`` and a font ``Comic Neue``.
+# This script for drawing icon is one-time used, but it is kept here for reference.
+# To run this script, you may need to install a python package ``drawsvg``.
 import drawsvg as dvg
 
 LIME_DARK = '#5E6100'
 LIME_LIGHT = '#A3A800'
-
-
-def write_text(*, x: int, y: int, width: int = 256) -> dvg.Group:
-    split_x = x + width * 0.57
-    transparent_line_1 = dvg.Line(x, y, split_x, y, opacity=0)
-    transparent_line_2 = dvg.Line(split_x, y, width, y, opacity=0)
-    text_recipes = [
-        ('beam', LIME_DARK, 'normal', transparent_line_1),
-        ('lime', LIME_LIGHT, 'bold', transparent_line_2),
-    ]
-    texts = [
-        dvg.Text(
-            text,
-            font_size=int((width / 4) * 0.958),
-            path=path,
-            text_anchor='start',
-            font_family='Comic Neue',
-            font_weight=weight,
-            fill=color,
-        )
-        for text, color, weight, path in text_recipes
-    ]
-    return dvg.Group(texts)
-
-
-def draw_beam(*, start_x: int = 0, y: int, width: int) -> dvg.Group:
-    beam_beam = dvg.Line(
-        start_x,
-        y,
-        start_x + width,
-        y,
-        stroke_width=3,
-        stroke=LIME_DARK,
-        stroke_linecap="round",
-    )
-    lime_beam = dvg.Line(
-        (start_x + width) * 0.66,
-        y,
-        start_x + width,
-        y,
-        stroke_width=3,
-        stroke=LIME_LIGHT,
-        stroke_linecap="round",
-    )
-    return dvg.Group([beam_beam, lime_beam])
 
 
 def draw_lime_splits(cx: int, cy: int, radius: int) -> dvg.Group:
@@ -64,7 +18,7 @@ def draw_lime_splits(cx: int, cy: int, radius: int) -> dvg.Group:
 
         lime_splits.append(
             dvg.Line(
-                cx, cy, x, y, stroke_width=4, stroke=LIME_LIGHT, stroke_linecap="round"
+                cx, cy, x, y, stroke_width=6, stroke=LIME_LIGHT, stroke_linecap="round"
             )
         )
         arc_rotation_r = radius * 0.8
@@ -80,10 +34,10 @@ def draw_lime_splits(cx: int, cy: int, radius: int) -> dvg.Group:
                 dvg.ArcLine(
                     np.cos(cur_theta + np.pi / 12) * arc_rotation_r + cx,
                     np.sin(cur_theta + np.pi / 12) * arc_rotation_r + cy,
-                    radius * 0.16,
+                    radius * 0.18,
                     start_angle_deg - (i + 1) * 45,
                     start_angle_deg - i * 45,
-                    stroke_width=2,
+                    stroke_width=5,
                     stroke=LIME_LIGHT,
                     stroke_linecap="round",
                     fill='none',
@@ -100,10 +54,10 @@ def draw_lime_splits(cx: int, cy: int, radius: int) -> dvg.Group:
                 dvg.ArcLine(
                     little_arc_x,
                     little_arc_y,
-                    radius * 0.16,
+                    radius * 0.18,
                     (-i + 2) * 45,
                     (-i + 3) * 45,
-                    stroke_width=2,
+                    stroke_width=5,
                     stroke=LIME_LIGHT,
                     stroke_linecap="round",
                     fill='none',
@@ -122,7 +76,7 @@ def draw_lime_arc(cx: int, cy: int, radius: int) -> dvg.Group:
                 radius,
                 -30,
                 150,
-                stroke_width=5,
+                stroke_width=8,
                 stroke=LIME_LIGHT,
                 stroke_linecap="round",
                 fill='none',
@@ -132,26 +86,13 @@ def draw_lime_arc(cx: int, cy: int, radius: int) -> dvg.Group:
 
 
 if __name__ == '__main__':
-    total_width = 300
-    total_height = int(total_width * 0.3)
-    d = dvg.Drawing(total_width, total_height, origin='top-left')
-    # Draw text
-    text_width = int(total_width * 0.85)
-
-    d.append(
-        draw_beam(
-            start_x=int(total_width * 0.07),
-            y=int(total_height * 0.4),
-            width=int(text_width * 0.95),
-        )
-    )
-    d.append(
-        write_text(
-            x=int(total_width * 0.02), y=int(total_height * 0.82), width=text_width
-        )
-    )
-    lime_radius = int(total_width * 0.075)
-    lime_cx, lime_cy = int(total_width * 0.88), int(total_height * 0.4)
+    d = dvg.Drawing(128, 128, origin='top-left')
+    lime_cx, lime_cy = 60, 76
+    lime_radius = 48
     d.append(draw_lime_splits(cx=lime_cx, cy=lime_cy, radius=lime_radius))
     d.append(draw_lime_arc(cx=lime_cx, cy=lime_cy, radius=lime_radius))
-    d.save_svg('logo.svg')
+    d.save_svg('icon.svg')
+    # After saving the image, convert the image with the following command
+    # convert -background transparent \
+    # -define icon:auto-resize="128,96,64,48,32,16" \
+    # icon.svg favicon.ico
