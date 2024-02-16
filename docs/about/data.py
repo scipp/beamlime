04969d0898c8e43b2906b5553fd12c54b4548f89
YooSunyoung <luysunyoung9@gmail.com>
2024-02-16 14:58:30 +0100

    Update checksum of benchmark results.

--- docs/about/data.py
+++ docs/about/data.py
@@ -14,7 +14,7 @@ def _make_pooch():
         retry_if_failed=3,
         base_url='https://public.esss.dk/groups/scipp/beamlime/benchmarks/',
         version=_version,
-        registry={'benchmark_results.json': 'md5:42d48ab2236adb809527664be2897bac'},
+        registry={'benchmark_results.json': 'md5:d217998889ce43133001da07c8efef9d'},
     )
 
 
