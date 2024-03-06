52b48b36abc6dcb397253a08c3ab49f1fc401015
Johannes Kasimir <johanneskasimir@gmail.com>
2024-03-06 10:31:01 +0100

    construct JSONGroup and pass in message

--- tests/applications/fake.py
+++ tests/applications/fake.py
@@ -1,19 +1,32 @@
+import os
+from typing import Union
+
+from scippneutron.io.load_nexus import json_nexus_group
+
 from beamlime import DaemonInterface
+
+Path = Union[str, bytes, os.PathLike]
 
 
 class FakeListener(DaemonInterface):
-
     messenger: MessageRouter
 
-    def __init__(self, run_start_message: dict):
-        self.run_start_message = run_start_message
+    def __init__(self, nexus_structure: dict):
+        self._group = json_nexus_group(nexus_structure)
+
+    @classmethod
+    def from_file(cls, path: Path):
+        '''Read nexus structure from json file'''
+        with open(path) as f:
+            nexus_structure = json.load(f)
+        return cls(nexus_structure)
 
     async def run(self) -> None:
         self.info("Fake data streaming started...")
 
         await self.messenger.send_message_async(
             RunStart(
-                content=self.run_start_message,
+                content=self._group,,
                 sender=self.__class__,
                 receiver=self.messenger.__class__,
             )
