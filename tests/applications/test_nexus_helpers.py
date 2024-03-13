10435aa95422c754a933f7af6e9a3273b0d822af
YooSunyoung <luysunyoung9@gmail.com>
2024-03-13 14:19:49 +0100

    Add random event generator tests.

--- tests/applications/test_nexus_helpers.py
+++ tests/applications/test_nexus_helpers.py
@@ -3,8 +3,22 @@
 import os
 
 from beamlime.applications._nexus_helpers import NexusContainer
+from beamlime.applications._random_data_providers import (
+    DetectorNumberCands,
+    random_ev44_generator,
+)
 
 
 def test_nexus_container_initialized_from_path():
     path = os.path.join(os.path.dirname(__file__), 'ymir.json')
     NexusContainer.from_template_file(path)
+
+
+def test_event_generator():
+    generator = random_ev44_generator(
+        detector_numbers=DetectorNumberCands(list(range(100)))
+    )
+    events = next(generator)
+    assert 99_900 < len(events) < 10_100
+    assert len(list(events['reference_time'])) == len(list(events['time_of_flight']))
+    assert len(list(events['reference_time'])) == len(list(events['pixel_id']))
