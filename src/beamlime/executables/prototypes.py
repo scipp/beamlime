8d2177eefd71feb924965375820cd7d73e41b142
YooSunyoung <luysunyoung9@gmail.com>
2024-03-14 13:51:34 +0100

    Update names.

--- src/beamlime/executables/prototypes.py
+++ src/beamlime/executables/prototypes.py
@@ -15,7 +15,7 @@ def collect_default_providers() -> ProviderGroup:
 
     from ..applications._parameters import collect_default_param_providers
     from ..applications.base import Application, MessageRouter
-    from ..applications.daemons import ESSKafkaStreamSimulator
+    from ..applications.daemons import FakeListener
     from ..applications.handlers import (
         DataReductionHandler,
         PlotSaver,
@@ -25,7 +25,7 @@ def collect_default_providers() -> ProviderGroup:
 
     app_providers = ProviderGroup(
         SingletonProvider(Application),
-        ESSKafkaStreamSimulator,
+        FakeListener,
         DataReductionHandler,
         PlotSaver,
         provide_stateless_workflow,
@@ -120,7 +120,7 @@ def run_standalone_prototype(
     from ..applications.base import Application
     from ..applications.daemons import (
         DetectorDataReceived,
-        ESSKafkaStreamSimulator,
+        FakeListener,
         NexusTemplatePath,
     )
     from ..applications.handlers import (
@@ -161,7 +161,7 @@ def run_standalone_prototype(
         )
 
         # Daemons
-        app.register_daemon(factory[ESSKafkaStreamSimulator])
+        app.register_daemon(factory[FakeListener])
         app.run()
 
 
