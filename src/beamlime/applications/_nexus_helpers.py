d9bb47add4c0be9d9f817a78bf693c6154fb234d
YooSunyoung <luysunyoung9@gmail.com>
2024-03-14 17:38:52 +0100

    Draft.

--- src/beamlime/applications/_nexus_helpers.py
+++ src/beamlime/applications/_nexus_helpers.py
@@ -2,9 +2,54 @@
 # Copyright (c) 2024 Scipp contributors (https://github.com/scipp)
 import os
 from functools import partial
-from typing import Any, Callable, Optional, Union
+from types import MappingProxyType
+from typing import Any, Callable, Literal, NamedTuple, Optional, Sequence, Union
+
+import numpy as np
 
 Path = Union[str, bytes, os.PathLike]
+
+
+def _nested_mapping_proxy(obj: dict) -> MappingProxyType:
+    """Create a nested shallow copy/mapping proxy of ``obj``.
+
+    If any value in the dictionary is a dictionary,
+    it is converted to a mapping proxy type.
+
+    Examples
+    --------
+    >>> from types import MappingProxyType
+    >>> _nested_mapping_proxy_type({'a': {'b': 1}})
+    mappingproxy({'a': mappingproxy({'b': 1})})
+
+    """
+    return MappingProxyType(
+        {
+            k: _nested_mapping_proxy(v) if isinstance(v, dict) else v
+            for k, v in obj.items()
+        }
+    )
+
+
+class DatasetRecipe(NamedTuple):
+    dtype: type
+    ndim: int
+
+
+FBIdentifier = Literal["ev44", "f144", "tdct"]
+ValuesRecipe = MappingProxyType[str, type]
+GROUP_RECIPES: MappingProxyType[FBIdentifier, ValuesRecipe] = _nested_mapping_proxy(
+    {
+        "f144": {"timestamp": DatasetRecipe(int, 0), "value": DatasetRecipe(float, 0)},
+        "tdct": {"name": DatasetRecipe(str, 0), "timestamps": DatasetRecipe(int, 1)},
+        "ev44": {
+            "reference_time": DatasetRecipe(float, 1),
+            "reference_time_index": DatasetRecipe(int, 1),
+            "time_of_flight": DatasetRecipe(float, 1),
+            "pixel_id": DatasetRecipe(int, 1),
+        },
+    }
+)
 
 
 def find_index(
@@ -66,6 +111,69 @@ def _match_dataset_name(child: dict, name: str) -> bool:
     )
 
 
+class ModularDatasetContainer:
+    """Modular dataset that can update/clear values."""
+
+    def __init__(self, name: str, recipe: DatasetRecipe) -> None:
+        self.recipe = recipe
+        self.config_dict: dict[str, Any]
+        self.config_dict = dict(name=name, dtype=recipe.dtype.__name__)
+        self.dataset_dict = dict(module='dataset', config=self.config_dict)
+
+    def update(self, new_values: Any) -> None:
+        """Update the values from the ``other``.
+
+        The values are extended if they are numpy.ndarray and replaced otherwise.
+        """
+        if 'values' not in self.config_dict or not isinstance(new_values, Sequence):
+            self.config_dict['values'] = new_values
+        elif isinstance((values := self.config_dict['values']), np.ndarray):
+            self.config_dict['values'] = np.append(values, new_values)
+        elif isinstance(values, list):
+            values.extend(new_values)
+
+
+class ModularDataGroupContainer:
+    """Module container protocol."""
+
+    def __init__(self, group_dict: dict, module_dict: dict, **initial_values) -> None:
+        self.sub_datasets: dict[str, ModularDatasetContainer]
+        self.module_dict = module_dict
+        self.group_dict = group_dict
+        self._populate_sub_dataset()
+        self.update(initial_values)
+
+    def _populate_sub_dataset(self) -> None:
+        """Populate the sub datasets from the module dictionary."""
+        module_id = self.module_dict['module']
+        dataset_recipes = GROUP_RECIPES[module_id]
+        self.group_dict['children'].remove(self.module_dict)
+        self.sub_datasets = {
+            name: ModularDatasetContainer(name, tp)
+            for name, tp in dataset_recipes.items()
+        }
+        self.group_dict['children'].extend(self.sub_datasets.values())
+
+    def update(self, other: dict) -> None:
+        """Update the sub datasets from the other. Ignore if the name is not present.
+
+        See :func:`~ModularDatasetContainer.update` for more details.
+        """
+        # If there is already a dataset with the same name, update it
+        # or populate new ones otherwise
+        for name, sub_dataset in self.sub_datasets.items():
+            if name in other:
+                sub_dataset.update(other[name])
+
+    def clear(self) -> None:
+        """Clear the sub datasets.
+
+        See :func:`~ModularDatasetContainer.clear` for more details.
+        """
+        # Remove all datasets from the group
+        ...
+
+
 class NXDetectorContainer:
     """NXDetector container.
 
@@ -110,7 +218,7 @@ class NexusContainer:
     def _collect_detectors(self, instrument_gr: dict) -> list[dict]:
         """Collect the detectors from the instrument group.
 
-        Only one or more NXdetectors are expected.
+        One or more NXdetectors are expected per NXinstrument.
         """
         return [
             child
@@ -122,7 +230,7 @@ class NexusContainer:
     def _get_instrument_group(self, nexus_container: dict) -> dict:
         """Get the NXinstrument group from the nexus container.
 
-        Only one NXinstrument group is expected.
+        Only one NXinstrument group is expected in one template.
         """
         return nested_dict_select(
             nexus_container,
