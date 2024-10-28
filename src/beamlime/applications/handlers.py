c1ae371dc7821aaa975f63125c5c69b03a30e234
Sunyoung Yoo <luysunyoung9@gmail.com>
2024-10-28 12:38:12 +0100

    Add application for continuous operation and raw detector view displays (#248)
    * Show detector application draft
    
    * Show detector application draft
    
    * Show detector application draft
    
    * Show detector application draft
    
    * Show detector application draft
    
    * Show detector application draft
    
    * Show detector application draft
    
    * Show detector application draft
    
    * Show detector application draft
    
    * Show detector application draft
    
    * Show detector application draft
    
    * Show detector application draft
    
    * Show detector application draft
    
    * Show detector application draft
    
    * Show detector application draft
    
    * Show detector application draft
    
    * Refactor event listener daemon.
    
    * Fix argument.
    
    * Handle keyboard interrupt.
    
    * Add dummy handlers.
    
    * Use plot-saver.
    
    * Use plot-saver.
    
    * Use plot-saver.
    
    * Use plot-saver.
    
    * Add dummy data reduction.
    
    * Add dummy data reduction.
    
    * Add dummy data reduction.
    
    * Add dummy data reduction.
    
    * Add dummy data reduction.
    
    * Add dummy data reduction.
    
    * Add dummy data reduction. [skip ci]
    
    * Add dummy data reduction. [skip ci]
    
    * Add dummy data reduction. [skip ci]
    
    * Add dummy data reduction. [skip ci]
    
    * Add dummy data reduction. [skip ci]
    
    * Add dummy data reduction. [skip ci]
    
    * Add dummy data reduction. [skip ci]
    
    * Add dummy data reduction. [skip ci]
    
    * Add dummy data reduction. [skip ci]
    
    * Remove place-holder for data assembling and plotting.
    
    * Add RawCountHandler
    
    * Fix arg-parser. [skip ci]
    
    * Add raw count handler [skip ci]
    
    * Handle multiple detectors and add fake clone det for testing
    
    * Use plain Matplotlib for faster plotting
    
    * Extract _plot_2d
    
    * Tweak plotting
    
    * Add missing time call.
    
    * Add static file path argument for a raw detector counter.
    
    * Add often-adjusted option.
    
    * Add fake listener flag.
    
    * Add normalize option for histograming.
    
    * Undo handlers change. [skip ci]
    
    * Undo handlers change. [skip ci]
    
    * Undo handlers change. [skip ci]
    
    * Undo handlers change. [skip ci]
    
    * Generalize by adding instrument and detector config
    
    * Define projection in config
    
    * Support logical views and multiple views per detector
    
    * Update raw detector view command
    
    Co-authored-by: Simon Heybrock <12912489+SimonHeybrock@users.noreply.github.com>
    
    * Fix imported package.
    
    ---------
    
    Co-authored-by: Simon Heybrock <simon.heybrock@ess.eu>
    Co-authored-by: Simon Heybrock <12912489+SimonHeybrock@users.noreply.github.com>

--- src/beamlime/applications/handlers.py
+++ src/beamlime/applications/handlers.py
@@ -2,16 +2,21 @@
 # Copyright (c) 2024 Scipp contributors (https://github.com/scipp)
 import argparse
 import pathlib
+import shutil
+import tempfile
 import time
 from dataclasses import dataclass
-from math import ceil
 from numbers import Number
 from typing import NewType
 
 import matplotlib.pyplot as plt
+import numpy as np
 import plopp as pp
 import scipp as sc
+import scippnexus as snx
+from ess.reduce.live import raw
 from ess.reduce.nexus.json_nexus import JSONGroup
+from matplotlib.gridspec import GridSpec
 
 from beamlime.logging import BeamlimeLogger
 
@@ -24,7 +29,7 @@ from ._nexus_helpers import (
     nexus_path_as_string,
 )
 from .base import HandlerInterface
-from .daemons import DataPieceReceived, RunStart
+from .daemons import DataPieceReceived, NexusFilePath, RunStart
 
 ResultRegistry = NewType("ResultRegistry", dict[str, sc.DataArray])
 """Workflow result container."""
@@ -71,6 +76,198 @@ def maxcount_or_maxtime(maxcount: Number, maxtime: Number):
             return True
 
     return run
+
+
+DETECTOR_BANK_SIZES = {
+    'larmor_detector': {'layer': 4, 'tube': 32, 'straw': 7, 'pixel': 512}
+}
+
+# NOTE: This config should obviously be moved/managed elsewhere, but this works for
+# for testing and is convenient for now.
+_res_scale = 8
+# Order in 'resolution' matters so plots have X as horizontal axis and Y as vertical.
+detector_registry = {}
+# The other DREAM detectors have non-consecutive detector numbers. This is not
+# supported currently
+_dream = {
+    'dashboard': {'nrow': 3, 'ncol': 2},
+    'detectors': {
+        'endcap_backward': {
+            'detector_name': 'endcap_backward_detector',
+            'resolution': {'y': 30 * _res_scale, 'x': 20 * _res_scale},
+            'gridspec': (0, 0),
+        },
+        'endcap_forward': {
+            'detector_name': 'endcap_forward_detector',
+            'resolution': {'y': 20 * _res_scale, 'x': 20 * _res_scale},
+            'gridspec': (0, 1),
+        },
+        # We use the arc length instead of phi as it makes it easier to get a correct
+        # aspect ratio for the plot if both axes have the same unit.
+        'mantle_projection': {
+            'detector_name': 'mantle_detector',
+            'resolution': {'arclength': 10 * _res_scale, 'z': 40 * _res_scale},
+            'projection': 'cylinder_mantle_z',
+            'gridspec': (1, slice(None, 2)),
+        },
+        # Different view of the same detector, showing just the front layer instead of
+        # a projection.
+        'mantle_front_layer': {
+            'detector_name': 'mantle_detector',
+            'projection': raw.LogicalView(
+                fold={
+                    'wire': 32,
+                    'module': 5,
+                    'segment': 6,
+                    'strip': 256,
+                    'counter': 2,
+                },
+                transpose=('wire', 'module', 'segment', 'counter', 'strip'),
+                select={'wire': 0},
+                flatten={'z_id': ('module', 'segment', 'counter')},
+            ),
+            'gridspec': (2, slice(None, 2)),
+        },
+    },
+}
+_loki = {
+    'dashboard': {'nrow': 3, 'ncol': 9, 'figsize_scale': 3},
+    'detectors': {
+        'Rear-detector': {
+            'detector_name': 'loki_detector_0',
+            'resolution': {'y': 12 * _res_scale, 'x': 12 * _res_scale},
+            'gridspec': (slice(0, 3), slice(0, 3)),
+            'pixel_noise': 'cylindrical',
+        },
+        # First window frame
+        'loki_detector_1': {
+            'detector_name': 'loki_detector_1',
+            'resolution': {'y': 3 * _res_scale, 'x': 9 * _res_scale},
+            'gridspec': (2, 4),
+            'pixel_noise': 'cylindrical',
+        },
+        'loki_detector_2': {
+            'detector_name': 'loki_detector_2',
+            'resolution': {'y': 9 * _res_scale, 'x': 3 * _res_scale},
+            'gridspec': (1, 3),
+            'pixel_noise': 'cylindrical',
+        },
+        'loki_detector_3': {
+            'detector_name': 'loki_detector_3',
+            'resolution': {'y': 3 * _res_scale, 'x': 9 * _res_scale},
+            'gridspec': (0, 4),
+            'pixel_noise': 'cylindrical',
+        },
+        'loki_detector_4': {
+            'detector_name': 'loki_detector_4',
+            'resolution': {'y': 9 * _res_scale, 'x': 3 * _res_scale},
+            'gridspec': (1, 5),
+            'pixel_noise': 'cylindrical',
+        },
+        # Second window frame
+        'loki_detector_5': {
+            'detector_name': 'loki_detector_5',
+            'resolution': {'y': 3 * _res_scale, 'x': 9 * _res_scale},
+            'gridspec': (2, 7),
+            'pixel_noise': 'cylindrical',
+        },
+        'loki_detector_6': {
+            'detector_name': 'loki_detector_6',
+            'resolution': {'y': 9 * _res_scale, 'x': 3 * _res_scale},
+            'gridspec': (1, 6),
+            'pixel_noise': 'cylindrical',
+        },
+        'loki_detector_7': {
+            'detector_name': 'loki_detector_7',
+            'resolution': {'y': 3 * _res_scale, 'x': 9 * _res_scale},
+            'gridspec': (0, 7),
+            'pixel_noise': 'cylindrical',
+        },
+        'loki_detector_8': {
+            'detector_name': 'loki_detector_8',
+            'resolution': {'y': 9 * _res_scale, 'x': 3 * _res_scale},
+            'gridspec': (1, 8),
+            'pixel_noise': 'cylindrical',
+        },
+    },
+}
+detector_registry['DREAM'] = _dream
+detector_registry['LoKI'] = _loki
+
+
+class RawCountHandler(HandlerInterface):
+    """
+    Continuously handle raw counts for every ev44 message.
+
+    This ignores run-start and run-stop messages.
+    """
+
+    def __init__(self, *, logger: BeamlimeLogger, nexus_file: NexusFilePath) -> None:
+        self.logger = logger
+        self._pulse = 0
+        self._previous: sc.DataArray | None = None
+        self._detectors: dict[str, list[str]] = {}
+        self._views: dict[str, raw.RollingDetectorView] = {}
+
+        with snx.File(nexus_file) as f:
+            entry = next(iter(f[snx.NXentry].values()))
+            instrument = next(iter(entry[snx.NXinstrument].values()))
+            self._instrument = str(instrument['name'][()])
+
+        for name, detector in detector_registry[self._instrument]['detectors'].items():
+            self._detectors.setdefault(detector['detector_name'], []).append(name)
+            self._views[name] = raw.RollingDetectorView.from_nexus(
+                nexus_file,
+                detector_name=detector['detector_name'],
+                window=100,
+                projection=detector.get('projection', 'xy_plane'),
+                resolution=detector.get('resolution'),
+                pixel_noise=detector.get('pixel_noise', sc.scalar(0.01, unit='m')),
+            )
+        self._chunk = {name: 0 for name in self._views}
+        self._buffer = {name: [] for name in self._views}
+        self.info("Initialized with %s", list(self._views))
+
+    def handle(self, message: DataPieceReceived) -> WorkflowResultUpdate | None:
+        detname = message.content.deserialized['source_name'].split('/')[-1]
+        event_id = message.content.deserialized['pixel_id']
+        if (det := self._detectors.get(detname)) is not None:
+            for name in det:
+                buffer = self._buffer[name]
+                buffer.append(event_id)
+                self._pulse += 1
+        else:
+            self.info("Ignoring data for %s", detname)
+            return
+        npulse = 10
+        if self._pulse % npulse == 0:
+            results = {}
+            for name, det in self._views.items():
+                buffer = self._buffer[name]
+                if len(buffer):
+                    det.add_counts(np.concatenate(buffer))
+                    buffer.clear()
+                for window in (50,):
+                    key = (self._instrument, name, f'window={window*npulse}')
+                    results[key] = det.get(window=window)
+            self.info("Publishing result for detectors %s", list(self._views))
+            return WorkflowResultUpdate(results)
+
+    @classmethod
+    def add_argument_group(cls, parser: argparse.ArgumentParser) -> None:
+        group = parser.add_argument_group("Raw Counter Configuration")
+        group.add_argument(
+            "--static-file-path",
+            help="Path to the nexus file that has static information.",
+            type=str,
+            required=True,
+        )
+
+    @classmethod
+    def from_args(
+        cls, logger: BeamlimeLogger, args: argparse.Namespace
+    ) -> "RawCountHandler":
+        return cls(logger=logger, nexus_file=args.static_file_path)
 
 
 class DataAssembler(HandlerInterface):
@@ -189,7 +386,7 @@ def random_image_path() -> ImagePath:
 
 
 MaxPlotColumn = NewType("MaxPlotColumn", int)
-DefaultMaxPlotColumn = MaxPlotColumn(2)
+DefaultMaxPlotColumn = MaxPlotColumn(3)
 
 
 class PlotStreamer(HandlerInterface):
@@ -238,6 +435,117 @@ class PlotStreamer(HandlerInterface):
         )
 
 
+def _plot_1d(
+    da: sc.DataArray,
+    *,
+    ax: plt.Axes,
+    title: str,
+    norm: str | None = None,
+    vmin: float | None = None,
+    vmax: float | None = None,
+    aspect: str | None = None,
+    **kwargs,
+):
+    x = sc.midpoints(da.coords[da.dims[0]]).values
+    ax.errorbar(x, da.values, yerr=np.sqrt(da.variances), fmt='o', **kwargs)
+    if norm is not None:
+        ax.set_yscale(norm)
+    y_min = vmin if vmin is not None else 0
+    y_max = vmax if vmax is not None else 1.05 * da.values.max()
+    ax.set_ylim(y_min, y_max)
+    ax.set_xlabel(f'{da.dims[0]} [{da.coords[da.dims[0]].unit}]')
+    ax.set_ylabel(f'[{da.unit}]')
+    ax.set_title(title)
+
+
+def plot_images_with_offsets(
+    *,
+    ax,
+    image_data_list,
+    x_coords_list,
+    y_coords_list,
+    cmap='viridis',
+    num_ticks=5,
+    **kwargs,
+):
+    import numpy as np
+
+    # Calculate common axis ranges
+    x_min = min(x.min() for x in x_coords_list)
+    x_max = max(x.max() for x in x_coords_list)
+    y_min = min(y.min() for y in y_coords_list)
+    y_max = max(y.max() for y in y_coords_list)
+
+    # Plot each image with correct extent
+    for image_data, x_coords, y_coords in zip(
+        image_data_list, x_coords_list, y_coords_list, strict=True
+    ):
+        extent = [x_coords.min(), x_coords.max(), y_coords.min(), y_coords.max()]
+        ax.imshow(
+            image_data,
+            extent=extent,
+            origin='lower',
+            interpolation='none',
+            cmap=cmap,
+            **kwargs,
+        )
+
+    # Set axis limits and ticks based on common ranges
+    ax.set_xlim(x_min, x_max)
+    ax.set_ylim(y_min, y_max)
+    ax.set_xticks(np.linspace(x_min, x_max, num=num_ticks))
+    ax.set_yticks(np.linspace(y_min, y_max, num=num_ticks))
+
+    # Set labels and title
+    ax.set_xlabel('X-axis')
+    ax.set_ylabel('Y-axis')
+    ax.set_title('Images Offset by Coordinate Arrays')
+
+
+def _plot_2d(
+    da: sc.DataArray | sc.DataGroup,
+    *,
+    ax: plt.Axes,
+    title: str,
+    **kwargs,
+):
+    if isinstance(da, sc.DataGroup):
+        das = list(da.values())
+        xname = das[0].dims[1]
+        yname = das[0].dims[0]
+        return plot_images_with_offsets(
+            ax=ax,
+            image_data_list=[da.values for da in das],
+            x_coords_list=[da.coords[xname].values for da in das],
+            y_coords_list=[da.coords[yname].values for da in das],
+            **kwargs,
+        )
+
+    x = da.coords.get(da.dims[1], sc.arange(da.dims[1], da.shape[1], unit=None))
+    y = da.coords.get(da.dims[0], sc.arange(da.dims[0], da.shape[0], unit=None))
+    if x.unit == y.unit:
+        aspect = float((y[-1] - y[0]) / (x[-1] - x[0]))
+        aspect *= da.shape[1] / da.shape[0]
+    else:
+        aspect = 'equal'
+
+    values = da.values
+    ax.imshow(values, **kwargs, aspect=aspect)
+    ax.invert_yaxis()
+    # Note: Drawing the colorbar actually takes a long time!
+    # cbar = plt.colorbar(ax.images[0], ax=ax)
+    # cbar.set_label(f'[{da.unit}]')
+    ax.set_title(title)
+    ax.set_xlabel(f'{da.dims[1]} [{x.unit}]')
+    ax.set_ylabel(f'{da.dims[0]} [{y.unit}]')
+    x_coords = x.values[::20]
+    ax.set_xticks(np.linspace(0, values.shape[1] - 1, len(x_coords)))
+    ax.set_xticklabels([round(x, 2) for x in x_coords])
+    y_coords = y.values[::20]
+    ax.set_yticks(np.linspace(0, values.shape[0] - 1, len(y_coords)))
+    ax.set_yticklabels([round(y, 2) for y in y_coords])
+
+
 class PlotSaver(PlotStreamer):
     """Plot handler to save the updated histogram into an image file."""
 
@@ -252,25 +560,38 @@ class PlotSaver(PlotStreamer):
         self.image_path_prefix = image_path_prefix
 
     def save_histogram(self, message: WorkflowResultUpdate) -> None:
+        start = time.time()
+
         image_file_name = f"{self.image_path_prefix}.png"
         self.info("Received histogram(s), saving into %s...", image_file_name)
 
-        fig, axes = plt.subplots(
-            ceil(len(message.content) / self.max_column),
-            self.max_column,
-            figsize=(8, 8),
-        )
-        plt.subplots_adjust(wspace=0.3, hspace=0.3)
-        for (name, da), ax in zip(message.content.items(), axes.flat, strict=False):
-            # TODO We need a way of configuring plot options for each item. The below
-            # works for the SANS 60387-2022-02-28_2215.nxs AgBeh file and is useful for
-            # testing.
-            if da.unit == '':
-                extra = {'norm': 'log', 'vmin': 1e-1, 'vmax': 1e1, 'aspect': 'equal'}
+        instrument = next(iter(message.content.keys()))[0]
+        dashboard = detector_registry[instrument]['dashboard']
+        ncol = dashboard['ncol']
+        nrow = dashboard['nrow']
+        figsize_scale = dashboard.get('figsize_scale', 6)
+
+        fig = plt.figure(figsize=(figsize_scale * ncol, figsize_scale * nrow))
+        gs = GridSpec(nrow, ncol, figure=fig)
+        for key, da in message.content.items():
+            instrument, detname, params = key
+            name = f"{detname} {params}"
+            grid_loc = detector_registry[instrument]['detectors'][detname]['gridspec']
+            ax = fig.add_subplot(gs[grid_loc])
+            extra = {'norm': 'log'}
+            if da.ndim == 2:
+                _plot_2d(da, ax=ax, title=name, **extra)
+            elif isinstance(da, sc.DataArray):
+                _plot_1d(da, ax=ax, title=name, **extra)
             else:
-                extra = {}
-            da.plot(ax=ax, title=name, **extra)
-        fig.savefig(image_file_name, dpi=100)
+                da.plot(ax=ax, title=name, **extra)
+        fig.tight_layout()
+        with tempfile.NamedTemporaryFile(suffix=".png", delete=False) as tmpfile:
+            # Saving into a tempfile avoids flickering when the image is updated, if
+            # the image is displayed in a GUI.
+            fig.savefig(tmpfile.name, dpi=150)
+            shutil.move(tmpfile.name, image_file_name)
+        self.info("Plotting took %.2f s", time.time() - start)
         plt.close(fig)
 
     @classmethod
---
+++ src/beamlime/executables/show_detector.py
@@ -0,0 +1,253 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2024 Scipp contributors (https://github.com/scipp)
+import argparse
+import json
+import pathlib
+from collections.abc import AsyncGenerator
+from typing import NewType
+
+from confluent_kafka import OFFSET_BEGINNING, Consumer, Message, TopicPartition
+from confluent_kafka.admin import AdminClient
+from streaming_data_types.eventdata_ev44 import deserialise_ev44
+
+from .. import Factory, ProviderGroup
+from ..applications._nexus_helpers import (
+    StreamModuleKey,
+    StreamModuleValue,
+)
+from ..applications.base import (
+    Application,
+    DaemonInterface,
+    MessageProtocol,
+    MessageRouter,
+)
+from ..applications.daemons import (
+    DataPiece,
+    DataPieceReceived,
+    DeserializedMessage,
+    FakeListener,
+)
+from ..applications.handlers import PlotSaver, RawCountHandler, WorkflowResultUpdate
+from ..constructors import SingletonProvider
+from ..constructors.providers import merge as merge_providers
+from ..logging import BeamlimeLogger
+from .options import build_minimum_arg_parser
+from .prototypes import fake_listener_from_args, instantiate_from_args
+
+KafkaConfig = NewType("KafkaConfig", dict)
+StreamingModules = NewType("StreamingModules", dict[StreamModuleKey, StreamModuleValue])
+
+
+def _mock_event_data_parent(path: str) -> dict:
+    return {
+        "name": pathlib.Path(path).name,
+        "type": "group",
+        "children": [],
+        "attributes": [{"name": "NX_class", "values": "NXevent_data"}],
+    }
+
+
+_ADMIN_SHARED_CONFIG_KEYS = (
+    "bootstrap.servers",
+    "security.protocol",
+    "sasl.mechanism",
+    "sasl.username",
+    "sasl.password",
+)
+
+
+def _collect_all_topic_partitions(
+    admin: AdminClient, topic: str
+) -> list[TopicPartition]:
+    """Retrieve the number of partitions for a given topic."""
+    topic_metadata = admin.list_topics(topic=topic).topics[topic]
+    return [
+        TopicPartition(topic, partition, OFFSET_BEGINNING)
+        for partition in topic_metadata.partitions.keys()
+    ]
+
+
+def _wrap_event_msg_to_data_piece(
+    topic: str, deserialized: DeserializedMessage
+) -> DataPiece:
+    key = StreamModuleKey(
+        module_type='ev44', topic=topic, source=deserialized['source_name']
+    )
+    return DataPiece(key=key, deserialized=deserialized)
+
+
+def _is_event_msg_valid(msg: Message) -> bool:
+    return (
+        msg is not None
+        and msg.error() is None
+        and (msg.value()[4:8].decode() == "ev44")
+    )
+
+
+class EventListener(DaemonInterface):
+    def __init__(
+        self,
+        *,
+        logger: BeamlimeLogger,
+        streaming_modules: StreamingModules,
+        kafka_config: KafkaConfig,
+    ) -> None:
+        self.logger = logger
+        self.info("Event topics: %s", {key.topic for key in streaming_modules.keys()})
+        self.streaming_modules = streaming_modules
+        admin_config = {key: kafka_config[key] for key in _ADMIN_SHARED_CONFIG_KEYS}
+        self.admin = AdminClient(admin_config)
+        try:
+            logger.info("Connecting to Kafka server.")
+            self.admin.list_topics(timeout=1)  # Check if the connection is successful.
+        except Exception as e:
+            err_msg = "Failed to connect to a Kafka server."
+            logger.error(err_msg)
+            raise RuntimeError(err_msg) from e
+
+        self.debug("Retrieving the number of partitions for each topic.")
+        self.topic_partitions = []
+        for topic in {key.topic for key in streaming_modules.keys()}:
+            self.topic_partitions += _collect_all_topic_partitions(self.admin, topic)
+        self.debug("Collected partitions: %s", self.topic_partitions)
+
+        self.consumer = Consumer(kafka_config)
+        self.consumer.assign(self.topic_partitions)
+
+    def __del__(self) -> None:
+        """Clean up the resources."""
+        self.info("Closing the Kafka consumer.")
+        if hasattr(self, 'consumer') and self.consumer is not None:
+            self.consumer.close()
+
+    async def run(self) -> AsyncGenerator[MessageProtocol | None, None]:
+        while True:
+            msg = self.consumer.poll(0.5)
+            if _is_event_msg_valid(msg):
+                deserialized = deserialise_ev44(msg.value())._asdict()
+                self.debug("%s", deserialized)
+                yield DataPieceReceived(
+                    content=_wrap_event_msg_to_data_piece(msg.topic(), deserialized)
+                )
+            elif msg is not None:
+                self.error("Unexpected message: %s", msg.value().decode())
+            yield None
+
+    @staticmethod
+    def add_argument_group(parser: argparse.ArgumentParser) -> None:
+        group = parser.add_argument_group('Event Listener Configuration')
+        group.add_argument(
+            "--config",
+            help="Path to the json file that has kafka configuration.",
+            type=str,
+        )
+
+    @classmethod
+    def from_args(
+        cls, logger: BeamlimeLogger, args: argparse.Namespace
+    ) -> "EventListener":
+        if args.config is None:
+            # This option is not set as a required argument in the `add_argument_group`
+            # method, because it is only required if fake listener is not used.
+            raise ValueError(
+                "The path to the config file is not provided."
+                "Use --config option to set it."
+            )
+
+        json_file_path = pathlib.Path(args.config)
+        config_dict = json.loads(json_file_path.read_text())
+        streaming_modules = streaming_modules_from_config(config_dict)
+        return EventListener(
+            logger=logger,
+            streaming_modules=StreamingModules(streaming_modules),
+            kafka_config=KafkaConfig(config_dict["kafka_config"]),
+        )
+
+
+def streaming_modules_from_config(config_dict: dict) -> StreamingModules:
+    streaming_modules_list = config_dict["streaming_modules"]
+    return StreamingModules(
+        {
+            StreamModuleKey(
+                module_type='ev44', topic=item['topic'], source=item['source']
+            ): StreamModuleValue(
+                path=item['path'], parent=_mock_event_data_parent(item['path'])
+            )
+            for item in streaming_modules_list
+        }
+    )
+
+
+def listener_from_args(
+    logger: BeamlimeLogger, args: argparse.Namespace
+) -> EventListener:
+    return instantiate_from_args(logger, args, EventListener)
+
+
+def raw_detector_counter_from_args(
+    logger: BeamlimeLogger, args: argparse.Namespace
+) -> RawCountHandler:
+    return instantiate_from_args(logger, args, RawCountHandler)
+
+
+def plot_saver_from_args(logger: BeamlimeLogger, args: argparse.Namespace) -> PlotSaver:
+    return instantiate_from_args(logger, args, PlotSaver)
+
+
+def collect_show_detector_providers() -> ProviderGroup:
+    from ..logging.providers import log_providers
+
+    app_providers = ProviderGroup(
+        listener_from_args,
+        raw_detector_counter_from_args,
+        fake_listener_from_args,
+        SingletonProvider(plot_saver_from_args),
+        SingletonProvider(ShowDetectorApp),
+        MessageRouter,
+    )
+
+    return merge_providers(log_providers, app_providers)
+
+
+class ShowDetectorApp(Application):
+    def run(self) -> None:
+        try:
+            super().run()
+        except KeyboardInterrupt:
+            self.info("Received a keyboard interrupt. Exiting...")
+            self.message_router.message_pipe.put_nowait(Application.Stop(content=None))
+
+
+def run_show_detector(factory: Factory, arg_name_space: argparse.Namespace) -> None:
+    factory[BeamlimeLogger].setLevel(arg_name_space.log_level.upper())
+    factory[BeamlimeLogger].info("Start showing detector hits.")
+    with factory.constant_provider(argparse.Namespace, arg_name_space):
+        if arg_name_space.fake_listener:
+            event_listener = factory[FakeListener]
+        else:
+            event_listener = factory[EventListener]
+
+        raw_detector_counter = factory[RawCountHandler]
+        plot_saver = factory[PlotSaver]
+        app = factory[ShowDetectorApp]
+
+    app.register_daemon(event_listener)
+    app.register_handling_method(DataPieceReceived, raw_detector_counter.handle)
+    app.register_handling_method(WorkflowResultUpdate, plot_saver.save_histogram)
+    app.run()
+
+
+def main() -> None:
+    """Entry point of the ``show-detector`` command."""
+    factory = Factory(collect_show_detector_providers())
+    arg_parser = build_minimum_arg_parser(
+        EventListener, PlotSaver, RawCountHandler, FakeListener
+    )
+    arg_parser.add_argument(
+        "--fake-listener",
+        action="store_true",
+        help="Use fake listener instead of real listener.",
+        default=False,
+    )
+    args = arg_parser.parse_args()
+    run_show_detector(factory, args)
