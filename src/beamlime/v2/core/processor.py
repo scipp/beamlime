b69ed38efb152c5dfea401998b7896f1105578e2
Simon Heybrock <simon.heybrock@ess.eu>
2024-12-11 08:20:25 +0100

    Rename to source and sink

--- src/beamlime/v2/core/processor.py
+++ src/beamlime/v2/core/processor.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 import logging
 from typing import Generic, Protocol
 
-from .handler import Consumer, HandlerRegistry, Producer, Tin, Tout
+from .handler import HandlerRegistry, MessageSink, MessageSource, Tin, Tout
 
 
 class Processor(Protocol):
@@ -18,19 +18,19 @@ class StreamProcessor(Generic[Tin, Tout]):
         self,
         *,
         logger: logging.Logger | None = None,
-        consumer: Consumer[Tin],
-        producer: Producer[Tout],
+        source: MessageSource[Tin],
+        sink: MessageSink[Tout],
         handler_registry: HandlerRegistry[Tin, Tout],
     ):
         self._logger = logger or logging.getLogger(__name__)
-        self._consumer = consumer
-        self._producer = producer
+        self._source = source
+        self._sink = sink
         self._handler_registry = handler_registry
 
     def process(self) -> None:
-        messages = self._consumer.get_messages()
+        messages = self._source.get_messages()
         results = []
         for msg in messages:
             handler = self._handler_registry.get(msg.topic)
             results.extend(handler.handle(msg))
-        self._producer.publish_messages(results)
+        self._sink.publish_messages(results)
