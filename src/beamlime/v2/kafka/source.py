0b99c8692b28747b5b0cab57a6caa955070edfa8
Simon Heybrock <simon.heybrock@ess.eu>
2024-12-12 12:15:51 +0100

    Add local demo service

--- src/beamlime/v2/kafka/source.py
+++ src/beamlime/v2/kafka/source.py
@@ -1,24 +1,27 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2024 Scipp contributors (https://github.com/scipp)
-from typing import Protocol
+from typing import Generic, Protocol, TypeVar
 
+from ..core.message import MessageSource
 from .message_adapter import KafkaMessage
 
+T = TypeVar("T")
 
-class KafkaConsumer(Protocol):
-    def consume(self, num_messages: int, timeout: float) -> list[KafkaMessage]:
+
+class KafkaConsumer(Protocol, Generic[T]):
+    def consume(self, num_messages: int, timeout: float) -> list[KafkaMessage[T]]:
         pass
 
 
-class KafkaMessageSource:
+class KafkaMessageSource(MessageSource[KafkaMessage[T]]):
     def __init__(
-        self, consumer: KafkaConsumer, num_messages: int = 100, timeout: float = 0.05
+        self, consumer: KafkaConsumer[T], num_messages: int = 100, timeout: float = 0.05
     ):
         self._consumer = consumer
         self._num_messages = num_messages
         self._timeout = timeout
 
-    def get_messages(self) -> list[KafkaMessage]:
+    def get_messages(self) -> list[KafkaMessage[T]]:
         return self._consumer.consume(
             num_messages=self._num_messages, timeout=self._timeout
         )
