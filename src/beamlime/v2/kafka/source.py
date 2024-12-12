d72fdac7769091fa020261f4bae1f77af183b2b2
Simon Heybrock <simon.heybrock@ess.eu>
2024-12-12 13:15:18 +0100

    Avoid extra adapter by using ev44 directly

--- src/beamlime/v2/kafka/source.py
+++ src/beamlime/v2/kafka/source.py
@@ -1,27 +1,25 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2024 Scipp contributors (https://github.com/scipp)
-from typing import Generic, Protocol, TypeVar
+from typing import Protocol
 
 from ..core.message import MessageSource
 from .message_adapter import KafkaMessage
 
-T = TypeVar("T")
 
-
-class KafkaConsumer(Protocol, Generic[T]):
-    def consume(self, num_messages: int, timeout: float) -> list[KafkaMessage[T]]:
+class KafkaConsumer(Protocol):
+    def consume(self, num_messages: int, timeout: float) -> list[KafkaMessage]:
         pass
 
 
-class KafkaMessageSource(MessageSource[KafkaMessage[T]]):
+class KafkaMessageSource(MessageSource[KafkaMessage]):
     def __init__(
-        self, consumer: KafkaConsumer[T], num_messages: int = 100, timeout: float = 0.05
+        self, consumer: KafkaConsumer, num_messages: int = 100, timeout: float = 0.05
     ):
         self._consumer = consumer
         self._num_messages = num_messages
         self._timeout = timeout
 
-    def get_messages(self) -> list[KafkaMessage[T]]:
+    def get_messages(self) -> list[KafkaMessage]:
         return self._consumer.consume(
             num_messages=self._num_messages, timeout=self._timeout
         )
--- src/beamlime/v2/services/local_demo_service.py
+++ src/beamlime/v2/services/local_demo_service.py
@@ -5,22 +5,24 @@ from typing import NoReturn
 
 import numpy as np
 import scipp as sc
+from streaming_data_types import eventdata_ev44
 
-from beamlime.v2.core.handler import HandlerRegistry, Message, MessageKey
+from beamlime.v2.core.handler import HandlerRegistry, Message
 from beamlime.v2.core.message import MessageSink
 from beamlime.v2.core.processor import StreamProcessor
 from beamlime.v2.core.service import Service
-from beamlime.v2.handlers.monitor_data_handler import MonitorDataHandler, MonitorEvents
+from beamlime.v2.handlers.monitor_data_handler import MonitorDataHandler
 from beamlime.v2.kafka.message_adapter import (
     AdaptingMessageSource,
+    ChainedAdapter,
+    Ev44ToMonitorEventsAdapter,
     FakeKafkaMessage,
-    KafkaMessage,
-    MessageAdapter,
+    KafkaToEv44Adapter,
 )
 from beamlime.v2.kafka.source import KafkaConsumer, KafkaMessageSource
 
 
-class FakeMonitorEventConsumer(KafkaConsumer[np.ndarray]):
+class FakeMonitorEventConsumer(KafkaConsumer):
     """Fake Kafka consumer that generates random monitor events."""
 
     def __init__(self):
@@ -29,27 +31,20 @@ class FakeMonitorEventConsumer(KafkaConsumer[np.ndarray]):
     def _make_normal(self, mean: float, std: float, size: int) -> np.ndarray:
         return self._rng.normal(loc=mean, scale=std, size=size).astype(np.int64)
 
-    def consume(
-        self, num_messages: int, timeout: float
-    ) -> list[FakeKafkaMessage[np.ndarray]]:
-        return [
-            FakeKafkaMessage(
-                value=self._make_normal(mean=30_000_000, std=10_000_000, size=10),
-                topic="monitors",
+    def consume(self, num_messages: int, timeout: float) -> list[FakeKafkaMessage]:
+        messages = []
+        for _ in range(num_messages):
+            time_of_flight = self._make_normal(mean=30_000_000, std=10_000_000, size=10)
+            ev44 = eventdata_ev44.serialise_ev44(
+                source_name="monitor1",
+                message_id=0,
+                reference_time=[time.time_ns()],
+                reference_time_index=[0],
+                time_of_flight=time_of_flight,
+                pixel_id=np.ones_like(time_of_flight, dtype=np.int32),
             )
-            for _ in range(num_messages)
-        ]
-
-
-class KafkaNumPyToMonitorEventsAdapter(
-    MessageAdapter[KafkaMessage[np.ndarray], Message[MonitorEvents]]
-):
-    def adapt(self, message: KafkaMessage[np.ndarray]) -> Message[MonitorEvents]:
-        return Message(
-            timestamp=time.time_ns(),
-            key=MessageKey(topic=message.topic(), source_name="monitor1"),
-            value=MonitorEvents(time_of_arrival=message.value()),
-        )
+            messages.append(FakeKafkaMessage(value=ev44, topic="monitors"))
+        return messages
 
 
 class PlotToPngSink(MessageSink[sc.DataArray]):
@@ -66,7 +61,9 @@ def main() -> NoReturn:
     processor = StreamProcessor(
         source=AdaptingMessageSource(
             source=KafkaMessageSource(consumer=FakeMonitorEventConsumer()),
-            adapter=KafkaNumPyToMonitorEventsAdapter(),
+            adapter=ChainedAdapter(
+                first=KafkaToEv44Adapter(), second=Ev44ToMonitorEventsAdapter()
+            ),
         ),
         sink=PlotToPngSink(),
         handler_registry=HandlerRegistry(
