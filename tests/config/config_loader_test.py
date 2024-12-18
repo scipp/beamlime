a76c0a12808fc5b0df9bfd30ee63a14e3a2295ce
Simon Heybrock <simon.heybrock@ess.eu>
2024-12-18 10:43:57 +0100

    Update test

--- tests/config/config_loader_test.py
+++ tests/config/config_loader_test.py
@@ -4,5 +4,5 @@ from beamlime.config.config_loader import load_config
 
 
 def test_load_consumer_config_loads_dev_env_by_default():
-    config = load_config(namespace='monitor_data', kind='consumer')
-    assert config['kafka']['bootstrap.servers'] == 'localhost:9092'
+    config = load_config(namespace='monitor_data')
+    assert config['consumer']['kafka']['bootstrap.servers'] == 'localhost:9092'
