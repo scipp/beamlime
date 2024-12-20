3b1947ddde7948d882c4127504e54d6af4b7d7a7
Simon Heybrock <simon.heybrock@ess.eu>
2024-12-20 09:31:59 +0100

    Make KafkaSink generic and add serializer support

--- src/beamlime/kafka/sink.py
+++ src/beamlime/kafka/sink.py
@@ -2,7 +2,7 @@
 # Copyright (c) 2024 Scipp contributors (https://github.com/scipp)
 import logging
 import time
-from typing import Any
+from typing import Any, Generic, Protocol, TypeVar
 
 import confluent_kafka as kafka
 import scipp as sc
@@ -11,15 +11,38 @@ from streaming_data_types import dataarray_da00
 from ..core.message import Message, MessageSink
 from .scipp_da00_compat import scipp_to_da00
 
+T = TypeVar("T")
 
-class KafkaSink(MessageSink[sc.DataArray]):
+
+class Serializer(Protocol, Generic[T]):
+    def __call__(self, value: Message[T]) -> bytes: ...
+
+
+def serialize_dataarray_to_da00(msg: Message[sc.DataArray]) -> bytes:
+    try:
+        da00 = dataarray_da00.serialise_da00(
+            source_name=msg.key.source_name,
+            timestamp_ns=time.time_ns(),
+            data=scipp_to_da00(msg.value),
+        )
+    except (ValueError, TypeError) as e:
+        raise RuntimeError(f"Failed to serialize message: {e}") from None
+    return da00
+
+
+class KafkaSink(MessageSink[T]):
     def __init__(
-        self, *, logger: logging.Logger | None = None, kafka_config: dict[str, Any]
+        self,
+        *,
+        logger: logging.Logger | None = None,
+        kafka_config: dict[str, Any],
+        serializer: Serializer[T] = serialize_dataarray_to_da00,
     ):
         self._logger = logger or logging.getLogger(__name__)
         self._producer = kafka.Producer(kafka_config)
+        self._serializer = serializer
 
-    def publish_messages(self, messages: Message[sc.DataArray]) -> None:
+    def publish_messages(self, messages: Message[T]) -> None:
         def delivery_callback(err, msg):
             if err is not None:
                 self._logger.error(
@@ -30,21 +53,17 @@ class KafkaSink(MessageSink[sc.DataArray]):
         for msg in messages:
             topic = msg.key.topic
             try:
-                da00 = dataarray_da00.serialise_da00(
-                    source_name=msg.key.source_name,
-                    timestamp_ns=time.time_ns(),
-                    data=scipp_to_da00(msg.value),
-                )
+                value = self._serializer(msg)
             except (ValueError, TypeError) as e:
                 self._logger.error("Failed to serialize message: %s", e)
-                continue
-            try:
-                self._producer.produce(
-                    topic=topic, value=da00, callback=delivery_callback
-                )
-                self._producer.poll(0)
-            except kafka.KafkaException as e:
-                self._logger.error("Failed to publish message to %s: %s", topic, e)
+            else:
+                try:
+                    self._producer.produce(
+                        topic=topic, value=value, callback=delivery_callback
+                    )
+                    self._producer.poll(0)
+                except kafka.KafkaException as e:
+                    self._logger.error("Failed to publish message to %s: %s", topic, e)
 
         try:
             self._producer.flush(timeout=3)
