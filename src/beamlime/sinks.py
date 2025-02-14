304fe0d7e6bce70db08e7e704061337930e45823
Mridul Seth <git@mriduls.com>
2025-02-14 12:09:19 +0100

    Make it work with fake detectors

--- src/beamlime/sinks.py
+++ src/beamlime/sinks.py
@@ -9,5 +9,6 @@ class PlotToPngSink(MessageSink[sc.DataArray]):
     def publish_messages(self, messages: Message[sc.DataArray]) -> None:
         for msg in compact_messages(messages):
             title = f"{msg.key.topic} - {msg.key.source_name}"
-            filename = f"{msg.key.topic}_{msg.key.source_name}.png"
+            # Normalize the source name to be a valid filename and not a directory
+            filename = f"{msg.key.topic}_{msg.key.source_name.replace('/', '_')}.png"
             msg.value.plot(title=title).save(filename)
