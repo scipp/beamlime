5cccdb66ab2bd1434eef4f9f27806268d8bcd906
Simon Heybrock <simon.heybrock@ess.eu>
2025-03-07 11:51:39 +0100

    Begin refactor to NXlog handler factory

--- src/beamlime/handlers/timeseries_handler.py
+++ src/beamlime/handlers/timeseries_handler.py
@@ -3,51 +3,49 @@
 from __future__ import annotations
 
 import logging
-from typing import TypeVar
+from typing import Any
 
 import scipp as sc
 
-from ..core.handler import Accumulator, Config, PeriodicAccumulatingHandler
+from ..core.handler import (
+    Accumulator,
+    Config,
+    Handler,
+    HandlerFactory,
+    PeriodicAccumulatingHandler,
+)
+from ..core.message import MessageKey
 from .accumulators import LogData
 
 
-class LogDataPreprocessor(Accumulator[LogData, sc.DataArray]):
+class ToNXlog(Accumulator[LogData, sc.DataArray]):
     """
     Preprocessor for log data.
 
-    Concatenates LogData objects and returns a single DataArray.
+    Concatenates LogData objects and returns a single DataArray as it would be read from
+    and NXlog in a NeXus file.
     """
 
-    def __init__(self) -> None:
+    # TODO Can we use ScippNexus here, with some hacks? We also need datetime handling,
+    # etc.
+    def __init__(self, attrs: dict[str, Any]) -> None:
+        self._attrs = attrs
         self._values: list[LogData] = []
 
     @property
-    def _unit(self) -> str:
-        if not self._values:
-            raise ValueError("No data has been added")
-        return self._values[0].unit
+    def _unit(self) -> str | None:
+        return self._attrs.get('unit')
 
     def add(self, timestamp: int, data: LogData) -> None:
-        if self._values:
-            if self._unit != data.unit:
-                raise ValueError("Cannot mix different units")
         self._values.append(data)
 
     def get(self) -> sc.DataArray:
-        if not self._values:
-            return sc.DataArray(
-                sc.array(dims=['time'], values=[], unit=''),
-                coords={'time': sc.array(dims=['time'], values=[], unit='ns')},
-            )
         values = [data.value for data in self._values]
         times = [data.time for data in self._values]
-        unit = self._unit
         self.clear()
         return sc.DataArray(
-            sc.array(dims=['time'], values=values, unit=unit),
-            coords={
-                'time': sc.array(dims=['time'], values=times, unit='ns'),
-            },
+            sc.array(dims=['time'], values=values, unit=self._unit),
+            coords={'time': sc.array(dims=['time'], values=times, unit='ns')},
         )
 
     def clear(self) -> None:
@@ -88,18 +86,42 @@ class Timeseries(Accumulator[sc.DataArray, sc.DataArray]):
         self._end = 0
 
 
-T = TypeVar('T')
+class LogdataHandlerFactory(HandlerFactory[LogData, sc.DataArray]):
+    """
+    Factory for creating handlers for log data.
 
+    This factory creates a handler that accumulates log data and returns it as a
+    DataArray.
+    """
 
-def create_logdata_handler(
-    *, logger: logging.Logger | None = None, config: Config
-) -> PeriodicAccumulatingHandler[T, sc.DataArray]:
-    """Create a handler for monitor data."""
-    accumulators = {'timeseries': Timeseries()}
-    preprocessor = LogDataPreprocessor()
-    return PeriodicAccumulatingHandler(
-        logger=logger,
-        config=config,
-        preprocessor=preprocessor,
-        accumulators=accumulators,
-    )
+    def __init__(
+        self,
+        *,
+        instrument: str,
+        logger: logging.Logger | None = None,
+        config: Config,
+        attribute_registry: dict[str, dict[str, any]],
+    ) -> None:
+        self._logger = logger or logging.getLogger(__name__)
+        self._config = config
+        self._instrument = instrument
+        self._attribute_registry = attribute_registry
+
+    def make_handler(self, key: MessageKey) -> Handler[LogData, sc.DataArray] | None:
+        # TODO Need to support not handling all messages. Return None?
+        source_name = key.source_name
+        attrs = self._attribute_registry.get(source_name)
+        if attrs is None:
+            self._logger.warning(
+                "No attributes found for source name %s. Messages will be dropped.",
+                source_name,
+            )
+            return None
+        preprocessor = ToNXlog(attrs=attrs)
+        accumulators = {'timeseries': Timeseries()}
+        return PeriodicAccumulatingHandler(
+            logger=self._logger,
+            config=self._config,
+            preprocessor=preprocessor,
+            accumulators=accumulators,
+        )
