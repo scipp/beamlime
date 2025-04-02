3003dfda58f7155f02b9b0abcb191fe3ebe1d327
Simon Heybrock <simon.heybrock@ess.eu>
2025-04-02 13:54:25 +0200

    Update remaining tests

--- tests/services/data_service_test.py
+++ tests/services/data_service_test.py
@@ -15,11 +15,7 @@ from beamlime.service_factory import DataServiceBuilder
 
 
 def fake_message_with_value(message: KafkaMessage, value: str) -> Message[str]:
-    return Message(
-        timestamp=1234,
-        key=MessageKey(topic=message.topic(), source_name="dummy"),
-        value=value,
-    )
+    return Message(timestamp=1234, key=MessageKey(source_name="dummy"), value=value)
 
 
 class ForwardingAdapter(MessageAdapter[KafkaMessage, Message[int]]):
