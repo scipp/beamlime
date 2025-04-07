37a7f9da6be5c28b93a739305f2b08f3f69c04f9
Simon Heybrock <simon.heybrock@ess.eu>
2025-04-07 06:41:20 +0200

    Cleanup

--- src/beamlime/kafka/routes.py
+++ src/beamlime/kafka/routes.py
@@ -31,64 +31,52 @@ class RoutingAdapterBuilder:
 
     def with_beam_monitor_route(self) -> Self:
         """Adds the beam monitor route."""
-        self._routes.update(_beam_monitor_route(self._stream_mapping))
+        adapter = RouteBySchemaAdapter(
+            routes={
+                'ev44': KafkaToMonitorEventsAdapter(
+                    stream_lut=self._stream_mapping.monitors
+                ),
+                'da00': ChainedAdapter(
+                    first=KafkaToDa00Adapter(
+                        stream_lut=self._stream_mapping.monitors,
+                        stream_kind=StreamKind.MONITOR_COUNTS,
+                    ),
+                    second=Da00ToScippAdapter(),
+                ),
+            }
+        )
+        for topic in self._stream_mapping.monitor_topics:
+            self._routes[topic] = adapter
         return self
 
     def with_detector_route(self) -> Self:
         """Adds the detector route."""
-        self._routes.update(_detector_route(self._stream_mapping))
+        adapter = ChainedAdapter(
+            first=KafkaToEv44Adapter(
+                stream_lut=self._stream_mapping.detectors,
+                stream_kind=StreamKind.DETECTOR_EVENTS,
+            ),
+            second=Ev44ToDetectorEventsAdapter(
+                merge_detectors=self._stream_mapping.instrument == 'bifrost'
+            ),
+        )
+        for topic in self._stream_mapping.detector_topics:
+            self._routes[topic] = adapter
         return self
 
     def with_logdata_route(self) -> Self:
         """Adds the logdata route."""
-        self._routes.update(_logdata_route(self._stream_mapping))
+        adapter = ChainedAdapter(
+            first=KafkaToF144Adapter(stream_lut=self._stream_mapping.logs),
+            second=F144ToLogDataAdapter(),
+        )
+        for topic in self._stream_mapping.log_topics:
+            self._routes[topic] = adapter
         return self
 
     def with_beamlime_config_route(self) -> Self:
         """Adds the beamlime config route."""
-        self._routes.update(_beamlime_config_route(self._stream_mapping))
+        self._routes[self._stream_mapping.beamline_config_topic] = (
+            BeamlimeConfigMessageAdapter()
+        )
         return self
-
-
-def _beamlime_config_route(stream_mapping: StreamMapping) -> dict[str, MessageAdapter]:
-    """Returns a dictionary containing the beamlime configuration route."""
-    return {stream_mapping.beamline_config_topic: BeamlimeConfigMessageAdapter()}
-
-
-def _beam_monitor_route(stream_mapping: StreamMapping) -> dict[str, MessageAdapter]:
-    """Returns a dictionary of routes for monitor data."""
-    adapter = RouteBySchemaAdapter(
-        routes={
-            'ev44': KafkaToMonitorEventsAdapter(stream_lut=stream_mapping.monitors),
-            'da00': ChainedAdapter(
-                first=KafkaToDa00Adapter(
-                    stream_lut=stream_mapping.monitors,
-                    stream_kind=StreamKind.MONITOR_COUNTS,
-                ),
-                second=Da00ToScippAdapter(),
-            ),
-        }
-    )
-    return {topic: adapter for topic in stream_mapping.monitor_topics}
-
-
-def _detector_route(stream_mapping: StreamMapping) -> dict[str, MessageAdapter]:
-    """Returns a dictionary of routes for detector data."""
-    adapter = ChainedAdapter(
-        first=KafkaToEv44Adapter(
-            stream_lut=stream_mapping.detectors, stream_kind=StreamKind.DETECTOR_EVENTS
-        ),
-        second=Ev44ToDetectorEventsAdapter(
-            merge_detectors=stream_mapping.instrument == 'bifrost'
-        ),
-    )
-    return {topic: adapter for topic in stream_mapping.detector_topics}
-
-
-def _logdata_route(stream_mapping: StreamMapping) -> dict[str, MessageAdapter]:
-    """Returns a dictionary of routes for log data."""
-    adapter = ChainedAdapter(
-        first=KafkaToF144Adapter(stream_lut=stream_mapping.logs),
-        second=F144ToLogDataAdapter(),
-    )
-    return {topic: adapter for topic in stream_mapping.log_topics}
