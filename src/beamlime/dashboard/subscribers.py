eaa8c8c0ba8678eb5af58aae84873adf12d24466
Simon Heybrock <simon.heybrock@ess.eu>
2025-06-12 15:02:24 +0200

    Hack data into the mockup

--- src/beamlime/dashboard/subscribers.py
+++ src/beamlime/dashboard/subscribers.py
@@ -3,7 +3,7 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
-from typing import Any
+from typing import Any, Literal
 
 import scipp as sc
 
@@ -28,12 +28,35 @@ class ComponentDataSubscriber(DataSubscriber):
 
     def __init__(self, *, component_key: ComponentDataKey, pipe: Pipe) -> None:
         self._cumulative_key = component_key.cumulative_key()
+        self._mode: Literal['cumulative', 'current'] = 'current'
         self._current_key = component_key.current_key()
         self._pipe = pipe
         super().__init__({self._cumulative_key, self._current_key})
 
+    @property
+    def mode(self) -> Literal['cumulative', 'current']:
+        """
+        The current mode of the subscriber, either 'cumulative' or 'current'.
+        """
+        return self._mode
+
+    @mode.setter
+    def mode(self, value: Literal['cumulative', 'current']) -> None:
+        """
+        Set the mode of the subscriber. This determines which data key is used for
+        sending data.
+        """
+        if value not in {'cumulative', 'current'}:
+            raise ValueError(
+                f"Invalid mode: {value}. Must be 'cumulative' or 'current'."
+            )
+        self._mode = value
+
     def send(self, data: dict[DataKey, Any]) -> None:
-        detector_data = RawData(
-            cumulative=data[self._cumulative_key], current=data[self._current_key]
-        )
-        self._pipe.send(detector_data)
+        if self._mode == 'cumulative':
+            da = data.get(self._cumulative_key)
+        elif self._mode == 'current':
+            da = data.get(self._current_key)
+        else:
+            return
+        self._pipe.send(da)
