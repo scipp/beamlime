892e414adf5bb4aac01418133e24bc5d5178c1f0
Simon Heybrock <simon.heybrock@ess.eu>
2025-06-16 13:51:12 +0200

    Add TOARangeParam

---
+++ src/beamlime/dashboard/detector_config.py
@@ -0,0 +1,20 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2025 Scipp contributors (https://github.com/scipp)
+import param
+
+
+class TOARangeParam(param.Parameterized):
+    enabled = param.Boolean(
+        default=False, doc="Enable the time of arrival range filter."
+    )
+    low = param.Number(
+        default=0.0, doc="Lower bound of the time window in microseconds."
+    )
+    high = param.Number(
+        default=72_000.0, doc="Upper bound of the time window in microseconds."
+    )
+    unit = param.Selector(
+        default='us',
+        objects=['ns', 'us', 'ms', 's'],
+        doc="Physical unit for time values.",
+    )
