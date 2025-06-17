c784cd16ca81b38249d9df342cc0fe510fec8fa7
Simon Heybrock <simon.heybrock@ess.eu>
2025-06-17 05:07:22 +0200

    Simplify tests

--- tests/dashboard/config_service_test.py
+++ tests/dashboard/config_service_test.py
@@ -2,48 +2,46 @@
 # Copyright (c) 2025 Scipp contributors (https://github.com/scipp)
 import pytest
 
-from beamlime.config.models import ConfigKey, TOARange
+from beamlime.config.models import TOARange
 from beamlime.dashboard.config_service import (
     ConfigSchemaManager,
     ConfigService,
     FakeMessageBridge,
 )
 from beamlime.dashboard.detector_config import TOARangeParam
-from beamlime.handlers.config_handler import ConfigUpdate
 
 
 @pytest.fixture
 def config_key():
-    return ConfigKey(service_name="detector_data", key="toa_range")
+    return "toa_range"
 
 
 @pytest.fixture
-def service(config_key: ConfigKey):
+def service(config_key: str):
     schemas = ConfigSchemaManager({config_key: TOARange})
     return ConfigService(schema_validator=schemas)
 
 
 @pytest.fixture
-def service_with_bridge(config_key: ConfigKey):
+def service_with_bridge(config_key: str):
     schemas = ConfigSchemaManager({config_key: TOARange})
     bridge = FakeMessageBridge()
     return ConfigService(schema_validator=schemas, message_bridge=bridge), bridge
 
 
 class TestConfigService:
-    def test_get_setter(self, service: ConfigService, config_key: ConfigKey):
+    def test_get_setter(self, service: ConfigService, config_key: str):
         setter = service.get_setter(config_key)
         setter(enabled=True, low=0.0, high=72_000.0, unit='us')
 
-    def test_subscriber(self, service_with_bridge, config_key: ConfigKey):
+    def test_subscriber(self, service_with_bridge, config_key: str):
         service, bridge = service_with_bridge
         toa_range = TOARangeParam()
         service.subscribe(key=config_key, callback=toa_range.param_updater())
 
         # Create a config update and add it to the bridge
         config_data = {'enabled': False, 'low': 1000.0, 'high': 2000.0, 'unit': 'us'}
-        update = ConfigUpdate(config_key=config_key, value=config_data)
-        bridge.add_incoming_message(update)
+        bridge.add_incoming_message((config_key, config_data))
 
         # Process the message
         service.process_incoming_messages()
