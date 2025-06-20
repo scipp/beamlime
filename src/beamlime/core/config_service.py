628f89b3f1e9212cd566a42774ac2e5d4d7f0b74
Simon Heybrock <simon.heybrock@ess.eu>
2025-06-20 12:25:19 +0200

    Comment on pending removal

--- src/beamlime/core/config_service.py
+++ src/beamlime/core/config_service.py
@@ -1,5 +1,9 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2024 Scipp contributors (https://github.com/scipp)
+"""
+ConfigService used by the legacy beamlime.services.dashboard module. Will be removed.
+"""
+
 import json
 import logging
 from typing import Any
