8c077c16031fdffaddf4f2014b1df11c4d183f90
Simon Heybrock <simon.heybrock@ess.eu>
2025-06-26 05:23:42 +0200

    Extract base class

--- src/beamlime/dashboard/data_streams.py
+++ src/beamlime/dashboard/data_streams.py
@@ -4,40 +4,48 @@
 Utilities for connecting subscribers to :py:class:`DataService`
 """
 
+from collections.abc import Hashable
+from typing import Any
+
 from holoviews import streams
 
-from .data_key import ComponentDataKey, DataKey, MonitorDataKey
+from .data_key import DataKey, MonitorDataKey
 from .data_service import DataService
-from .data_subscriber import DataSubscriber
+from .data_subscriber import DataSubscriber, StreamAssembler
 from .subscribers import ComponentStreamAssembler, MergingStreamAssembler
 
 
-class MonitorStreamManager:
-    """A manager for monitor data streams."""
+class StreamManager:
+    """Base class for managing data streams."""
 
     def __init__(self, data_service: DataService):
         self.data_service = data_service
-        self.pipes: dict[ComponentDataKey, streams.Pipe] = {}
+        self.pipes: dict[Any, streams.Pipe] = {}
+
+    def _get_or_create_stream(
+        self, key: Hashable, assembler: StreamAssembler
+    ) -> streams.Pipe:
+        """Get or create a stream for the given key and assembler."""
+        if key not in self.pipes:
+            pipe = streams.Pipe(data=None)
+            subscriber = DataSubscriber(assembler, pipe)
+            self.data_service.register_subscriber(subscriber)
+            self.pipes[key] = pipe
+        return self.pipes[key]
+
+
+class MonitorStreamManager(StreamManager):
+    """A manager for monitor data streams."""
 
     def get_stream(self, component_name: str) -> streams.Pipe:
         """Get or create a data stream for the given component key."""
-        # Currently there are now views for monitor data, so we use an empty view name.
         data_key = MonitorDataKey(component_name=component_name, view_name='')
-        if data_key not in self.pipes:
-            pipe = streams.Pipe(data=None)
-            assembler = ComponentStreamAssembler(data_key)
-            subscriber = DataSubscriber(assembler, pipe)
-            self.data_service.register_subscriber(subscriber)
-            self.pipes[data_key] = pipe
-        return self.pipes[data_key]
+        assembler = ComponentStreamAssembler(data_key)
+        return self._get_or_create_stream(data_key, assembler)
 
 
-class ReductionStreamManager:
+class ReductionStreamManager(StreamManager):
     """A manager for reduction data streams."""
-
-    def __init__(self, data_service: DataService):
-        self.data_service = data_service
-        self.pipes: dict[tuple[tuple[str, ...], str], streams.Pipe] = {}
 
     def get_stream(self, source_names: set[str], view_name: str) -> streams.Pipe:
         """Get or create a data stream for the given component key and view."""
@@ -49,11 +57,6 @@ class ReductionStreamManager:
             )
             for source_name in source_names
         }
-        key = (tuple(source_names), view_name)
-        if key not in self.pipes:
-            pipe = streams.Pipe(data=None)
-            assembler = MergingStreamAssembler(data_keys)
-            subscriber = DataSubscriber(assembler, pipe)
-            self.data_service.register_subscriber(subscriber)
-            self.pipes[key] = pipe
-        return self.pipes[key]
+        key = (tuple(sorted(source_names)), view_name)
+        assembler = MergingStreamAssembler(data_keys)
+        return self._get_or_create_stream(key, assembler)
