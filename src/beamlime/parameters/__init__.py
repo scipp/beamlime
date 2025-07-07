0130d21bfd3f24c65041310fb1354e40563e3d5c
Simon Heybrock <simon.heybrock@ess.eu>
2025-07-07 08:33:39 +0200

    Remove now unused ParameterRegistry

--- src/beamlime/parameters/__init__.py
+++ src/beamlime/parameters/__init__.py
@@ -1,10 +1,2 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2025 Scipp contributors (https://github.com/scipp)
-
-from .parameter_registry import ModelId, ParameterRegistry, get_parameter_registry
-
-__all__ = [
-    'ModelId',
-    'ParameterRegistry',
-    'get_parameter_registry',
-]
