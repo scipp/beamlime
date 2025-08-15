05e0e5ceb8aac83c47c59503bb8b9f4a1da48b88
Simon Heybrock <simon.heybrock@ess.eu>
2025-08-15 08:41:01 +0200

    Fix tests using instrument filter

--- tests/services/data_reduction_test.py
+++ tests/services/data_reduction_test.py
@@ -222,7 +222,7 @@ def test_service_can_recover_after_bad_workflow_id_was_set(
         source_name='panel_0', service_name="data_reduction", key="workflow_config"
     )
     bad_workflow_id = workflow_spec.WorkflowConfig(
-        identifier='abcde12345',  # Invalid workflow ID
+        identifier='dummy/abcde12345',  # Invalid workflow ID
     )
     # Trigger workflow start
     app.publish_config_message(key=config_key, value=bad_workflow_id.model_dump())
@@ -254,7 +254,7 @@ def test_service_can_recover_after_bad_workflow_param_was_set(
     app = make_reduction_app(instrument='dummy')
     sink = app.sink
     service = app.service
-    workflow_id, spec = _get_workflow_from_registry('dummy', 'total_counts')
+    workflow_id, _ = _get_workflow_from_registry('dummy', 'total_counts')
 
     config_key = ConfigKey(
         source_name='panel_0', service_name="data_reduction", key="workflow_config"
@@ -311,7 +311,7 @@ def test_active_workflow_keeps_running_when_bad_workflow_id_or_params_were_set(
 
     # Try to set an invalid workflow ID
     bad_workflow_id = workflow_spec.WorkflowConfig(
-        identifier='abcde12345',  # Invalid workflow ID
+        identifier='dummy/abcde12345',  # Invalid workflow ID
         params={},
     )
     app.publish_config_message(key=config_key, value=bad_workflow_id.model_dump())
