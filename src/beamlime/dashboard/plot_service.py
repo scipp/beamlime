9b5f306da9b868fe332b39fd6ba562581dcf0926
Simon Heybrock <simon.heybrock@ess.eu>
2025-09-02 12:56:15 +0200

    Add PlotService

---
+++ src/beamlime/dashboard/plot_service.py
@@ -0,0 +1,79 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2025 Scipp contributors (https://github.com/scipp)
+from __future__ import annotations
+
+import logging
+from collections.abc import Hashable
+from typing import Protocol, TypeVar
+
+import holoviews as hv
+import scipp as sc
+
+from beamlime.config.workflow_spec import JobId, JobNumber, ResultKey, WorkflowId
+
+from .job_service import JobService
+from .stream_manager import StreamManager
+
+K = TypeVar('K', bound=Hashable)
+V = TypeVar('V')
+
+
+class Plot(Protocol):
+    def __init__(self) -> None: ...
+
+    def __call__(
+        self, data: sc.DataArray | dict[ResultKey, sc.DataArray]
+    ) -> hv.Element: ...
+
+
+class PlotService:
+    def __init__(
+        self,
+        job_service: JobService,
+        stream_manager: StreamManager,
+        logger: logging.Logger | None = None,
+    ) -> None:
+        self._job_service = job_service
+        self._stream_manager = stream_manager
+        self._logger = logger or logging.getLogger(__name__)
+        self._plot_fns: dict[
+            tuple[WorkflowId, str | None],
+            list[type[Plot]],
+        ] = {}
+
+    def register_plot(
+        self, workflow_id: WorkflowId, output_name: str | None, plot_cls: type[Plot]
+    ) -> None:
+        # output_name = None means the workflow produces a single output
+        key = (workflow_id, output_name)
+        plots = self._plot_fns.setdefault(key, [])
+        plots.append(plot_cls)
+
+    def get_available_plots(
+        self, job_number: JobNumber, output_name: str | None
+    ) -> list[Plot]:
+        """Get all available plots for a given job and output."""
+        workflow_id = self._job_service.job_info[job_number]
+        key = (workflow_id, output_name)
+        plot_classes = self._plot_fns.get(key, [])
+        return [plot_cls() for plot_cls in plot_classes]
+
+    def create_plot(
+        self,
+        job_number: JobNumber,
+        source_names: list[str],
+        output_name: str | None,
+        plot: Plot,
+    ) -> hv.DynamicMap:
+        workflow_id = self._job_service.job_info[job_number]
+        keys = {
+            ResultKey(
+                workflow_id=workflow_id,
+                job_id=JobId(job_number=job_number, source_name=source_name),
+                output_name=output_name,
+            )
+            for source_name in source_names
+        }
+        pipe = self._stream_manager.make_merging_stream(keys)
+        dmap = hv.DynamicMap(plot, streams=[pipe], cache_size=1).opts(shared_axes=False)
+        return dmap
