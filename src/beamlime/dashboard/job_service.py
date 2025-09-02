a8bd0b050e0daece9f4026aa5ebd3e9a21d2daa3
Simon Heybrock <simon.heybrock@ess.eu>
2025-09-02 09:39:59 +0200

    Group by JobNumber

--- src/beamlime/dashboard/job_service.py
+++ src/beamlime/dashboard/job_service.py
@@ -8,7 +8,7 @@ from typing import TypeVar
 
 import scipp as sc
 
-from beamlime.config.workflow_spec import JobId, ResultKey, WorkflowId
+from beamlime.config.workflow_spec import JobId, JobNumber, ResultKey, WorkflowId
 
 from .data_service import DataService
 
@@ -17,12 +17,14 @@ V = TypeVar('V')
 
 # Next:
 # - Remove WorkflowConfig.identifier = None "stop mechanism"
-# - Pass WorkflowConfigService to JobService, so we can send stop messages
 # - Move some methods from WorkflowController to JobService
 # - WorkflowController is for getting workflows and launching jobs
 # - JobService is for tracking jobs and stopping them
 # - Should we have a separate JobController?
 # - Make a new widgets or refactor WorkflowStatusListWidget
+
+SourceName = str
+SourceData = sc.DataArray | dict[str, sc.DataArray]
 
 
 class JobService:
@@ -34,41 +36,48 @@ class JobService:
     ) -> None:
         self._logger = logger or logging.getLogger(__name__)
         self._data_service = data_service
-        self._job_data: dict[JobId, sc.DataArray | sc.DataGroup] = {}
-        self._job_info: dict[JobId, WorkflowId] = {}
+        self._job_data: dict[JobNumber, dict[SourceName, SourceData]] = {}
+        self._job_info: dict[JobNumber, WorkflowId] = {}
         self._data_service.register_subscriber(self.data_updated)
+
+    @property
+    def job_data(self) -> dict[JobNumber, dict[SourceName, SourceData]]:
+        return self._job_data
+
+    @property
+    def job_info(self) -> dict[JobNumber, WorkflowId]:
+        return self._job_info
 
     def data_updated(self, updated_keys: set[ResultKey]) -> None:
         notify_job_update = False
         for key in updated_keys:
             value = self._data_service[key]
-            # If we have multiple outputs then multiple updated keys will update this,
-            # but they should all have the same workflow_id for the same job_id. Since
-            # currently JobId is not unique across backend restarts we cannot raise on
-            # mismatch, but we log a warning if it happens.
-            if (workflow_id := self._job_info.get(key.job_id)) is None:
-                self._job_info[key.job_id] = key.workflow_id
+            job_number = key.job_id.job_number
+            source_name = key.job_id.source_name
+            if (workflow_id := self._job_info.get(job_number)) is None:
+                self._job_info[job_number] = key.workflow_id
                 notify_job_update = True
             elif workflow_id != key.workflow_id:
                 self._logger.warning(
-                    "Workflow ID mismatch for job %s: existing %s, new %s. "
-                    "This may happen if the backend was restarted.",
+                    "Workflow ID mismatch for job %s: existing %s, new %s. ",
                     key.job_id,
                     workflow_id,
                     key.workflow_id,
                 )
+            all_source_data = self._job_data.setdefault(job_number, {})
+            # Store in DataService for access by plots etc.
             if key.output_name is None:
                 # Single output, store directly
-                self._job_data[key.job_id] = value
+                all_source_data[source_name] = value
             else:
-                # Multiple outputs, store in a DataGroup
-                job_data = self._job_data.setdefault(key.job_id, sc.DataGroup())
-                if not isinstance(job_data, sc.DataGroup):
+                # Multiple outputs, store in a dict
+                source_data = all_source_data.setdefault(source_name, dict)
+                if not isinstance(source_data, dict):
                     # This should never happen in practice, but we check to be safe
                     raise ValueError(
-                        f"Expected DataGroup for job {key.job_id}, got {type(job_data)}"
+                        f"Expected dict for multiple outputs, got {type(source_data)}"
                     )
-                job_data[key.output_name] = value
+                source_data[key.output_name] = value
         if notify_job_update:
             self._notify_job_update()
 
@@ -77,3 +86,7 @@ class JobService:
         # For now we just log the job updates. In future we may want to have a more
         # sophisticated notification mechanism.
         self._logger.info("Job data updated for jobs: %s", list(self._job_info.keys()))
+
+    # To move from WorkflowController
+    def stop_job(self, job: JobId) -> None: ...
+    def remove_job(self, job: JobId) -> None: ...
