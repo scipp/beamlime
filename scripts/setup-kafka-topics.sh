3c860ddc20c0eebde064f5def1f908a52724a34d
Simon Heybrock <simon.heybrock@ess.eu>
2025-09-11 04:46:47 +0200

    Update topic script

--- scripts/setup-kafka-topics.sh
+++ scripts/setup-kafka-topics.sh
@@ -2,17 +2,17 @@
 set -e
 
 if [ -z "$1" ]; then
-  echo "Usage: $0 <BEAMLIME_INSTRUMENT>"
+  echo "Usage: $0 <LIVEDATA_INSTRUMENT>"
   exit 1
 fi
 
-BEAMLIME_INSTRUMENT=$1
+LIVEDATA_INSTRUMENT=$1
 
 kafka-topics --bootstrap-server kafka:29092 --delete --topic '.*' || true
 kafka-leader-election --bootstrap-server kafka:29092 --election-type PREFERRED --all-topic-partitions
 
 kafka-topics --create --bootstrap-server kafka:29092 \
-  --topic ${BEAMLIME_INSTRUMENT}_detector \
+  --topic ${LIVEDATA_INSTRUMENT}_detector \
   --config cleanup.policy=delete \
   --config delete.retention.ms=60000 \
   --config max.message.bytes=104857600 \
@@ -22,7 +22,7 @@ kafka-topics --create --bootstrap-server kafka:29092 \
   --config segment.ms=60000
 
 kafka-topics --create --bootstrap-server kafka:29092 \
-  --topic ${BEAMLIME_INSTRUMENT}_beam_monitor \
+  --topic ${LIVEDATA_INSTRUMENT}_beam_monitor \
   --config cleanup.policy=delete \
   --config delete.retention.ms=60000 \
   --config max.message.bytes=104857600 \
@@ -32,7 +32,7 @@ kafka-topics --create --bootstrap-server kafka:29092 \
   --config segment.ms=60000
 
 kafka-topics --create --bootstrap-server kafka:29092 \
-  --topic ${BEAMLIME_INSTRUMENT}_motion \
+  --topic ${LIVEDATA_INSTRUMENT}_motion \
   --config cleanup.policy=delete \
   --config delete.retention.ms=60000 \
   --config max.message.bytes=104857600 \
@@ -42,7 +42,7 @@ kafka-topics --create --bootstrap-server kafka:29092 \
   --config segment.ms=60000
 
 kafka-topics --create --bootstrap-server kafka:29092 \
-  --topic ${BEAMLIME_INSTRUMENT}_livedata_commands \
+  --topic ${LIVEDATA_INSTRUMENT}_livedata_commands \
   --config cleanup.policy=compact \
   --config retention.ms=-1 \
   --config min.compaction.lag.ms=86400000 \
@@ -53,7 +53,7 @@ kafka-topics --create --bootstrap-server kafka:29092 \
 
 # Status/heartbeat topic
 kafka-topics --create --bootstrap-server kafka:29092 \
-  --topic ${BEAMLIME_INSTRUMENT}_livedata_heartbeat \
+  --topic ${LIVEDATA_INSTRUMENT}_livedata_heartbeat \
   --config cleanup.policy=delete \
   --config retention.ms=60000 \
   --config min.compaction.lag.ms=86400000 \
@@ -63,7 +63,7 @@ kafka-topics --create --bootstrap-server kafka:29092 \
   --config retention.bytes=-1
 
 kafka-topics --create --bootstrap-server kafka:29092 \
-  --topic ${BEAMLIME_INSTRUMENT}_livedata_data \
+  --topic ${LIVEDATA_INSTRUMENT}_livedata_data \
   --config cleanup.policy=delete \
   --config delete.retention.ms=60000 \
   --config max.message.bytes=104857600 \
