7073844bc38c444455fbc128c807e710d5e8ff3d
Simon Heybrock <simon.heybrock@ess.eu>
2025-09-19 10:54:06 +0200

    Revert "Update src/ess/livedata/dashboard/widgets/correlation_histogram_widget.py"
    This reverts commit 496ef7c06957c099c213d1d3730f5389d11e5bd7.

--- src/ess/livedata/dashboard/widgets/correlation_histogram_widget.py
+++ src/ess/livedata/dashboard/widgets/correlation_histogram_widget.py
@@ -61,9 +61,11 @@ class CorrelationHistogramWidget:
                     'output_name': key.output_name or '',
                     'result_key': key,
                 }
-                for key in set(timeseries_keys) - {'result_key'}
+                for key in timeseries_keys
             ]
         )
+        if timeseries_keys:
+            self._result_key = data.pop('result_key')
         self._tabulator.value = data
         self._on_selection_change()  # Update button state
 
