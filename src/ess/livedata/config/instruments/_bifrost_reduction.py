f8c3c8ebd3d2c4a32069133268d94451b53c794b
Simon Heybrock <simon.heybrock@ess.eu>
2025-09-19 06:54:20 +0200

    Begin adding Bifrost Q-map/cut workflow

---
+++ src/ess/livedata/config/instruments/_bifrost_reduction.py
@@ -0,0 +1,104 @@
+"""
+Bifrost with all banks merged into a single one.
+"""
+
+from collections.abc import Generator
+from typing import NewType
+
+import pydantic
+import scipp as sc
+from scippnexus import NXdetector
+
+from ess.livedata.config import Instrument, instrument_registry
+from ess.livedata.config.env import StreamingEnv
+from ess.livedata.config.workflows import register_monitor_timeseries_workflows
+from ess.livedata.handlers.detector_data_handler import (
+    DetectorLogicalView,
+    LogicalViewConfig,
+    get_nexus_geometry_filename,
+)
+from ess.livedata.handlers.stream_processor_workflow import StreamProcessorWorkflow
+from ess.reduce.nexus.types import (
+    CalibratedBeamline,
+    DetectorData,
+    Filename,
+    NeXusData,
+    NeXusName,
+    SampleRun,
+)
+from ess.spectroscopy.indirect.time_of_flight import TofWorkflow
+from ess.spectroscopy.types import InstrumentAngle, SampleAngle
+from ess.bifrost import BifrostWorkflow
+from ess.bifrost.live import CutAxis, CutAxis1, CutAxis2, CutData, BifrostQCutWorkflow
+
+from ._ess import make_common_stream_mapping_inputs, make_dev_stream_mapping
+import scipp as sc
+import sciline
+import time
+from ess.spectroscopy.types import *
+import scippnexus as snx
+
+from ess.bifrost.data import (
+    simulated_elastic_incoherent_with_phonon,
+    tof_lookup_table_simulation,
+)
+
+fname = simulated_elastic_incoherent_with_phonon()
+with snx.File(fname) as f:
+    detector_names = list(f['entry/instrument'][snx.NXdetector])
+detector_names = detector_names[:2]
+
+workflow = BifrostQCutWorkflow(detector_names)
+workflow[Filename[SampleRun]] = simulated_elastic_incoherent_with_phonon()
+workflow[TimeOfFlightLookupTableFilename] = tof_lookup_table_simulation()
+workflow[PreopenNeXusFile] = PreopenNeXusFile(True)
+
+workflow[CutAxis1] = CutAxis.from_q_vector(
+    output="Qx",
+    vec=sc.vector([1, 0, 0]),
+    bins=sc.linspace('Qx', -3.0, 3.0, 300, unit='1/Å'),
+)
+
+workflow[CutAxis2] = CutAxis.from_q_vector(
+    output="Qz",
+    vec=sc.vector([0, 0, 1]),
+    bins=sc.linspace('Qz', -3.0, 3.0, 300, unit='1/Å'),
+)
+
+
+workflow[CutAxis1] = CutAxis(
+    output="|Q|",
+    fn=lambda sample_table_momentum_transfer: sc.norm(sample_table_momentum_transfer),
+    bins=sc.linspace('|Q|', 0.9, 3.0, 100, unit='1/Å'),
+)
+
+workflow[CutAxis2] = CutAxis(
+    output="E",
+    fn=lambda energy_transfer: energy_transfer,
+    bins=sc.linspace('E', -0.1, 0.1, 100, unit='meV'),
+)
+
+
+def register_qcut_workflows(
+    instrument: Instrument,
+) -> None:
+    @instrument.register_workflow(
+        name='qmap',
+        version=1,
+        title='Q map',
+        description='',
+        source_names=['unified_detector'],
+        aux_source_names=['detector_rotation', 'sample_rotation'],
+    )
+    def _spectrum_view() -> StreamProcessorWorkflow:
+        wf = workflow.copy()
+        return StreamProcessorWorkflow(
+            wf,
+            dynamic_keys={'unified_detector': NeXusData[NXdetector, SampleRun]},
+            context_keys={
+                'detector_rotation': InstrumentAngle[SampleRun],
+                'sample_rotation': SampleAngle[SampleRun],
+            },
+            target_keys=(CutData[SampleRun],),
+            accumulators=(CutData[SampleRun],),
+        )
