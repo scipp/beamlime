bfee729c4036a1fc78d9117a708b6a9f4917cd23
YooSunyoung <17974113+YooSunYoung@users.noreply.github.com>
2025-09-22 13:05:30 +0200

    Minimum script for starting odin services.

---
+++ odin-demo.sh
@@ -0,0 +1,30 @@
+# Configuring kafka environment variables
+export KAFKA_SALS_USERNAME=DUMMY
+export KAFKA_SALS_PASSWORD=DUMMY
+export KAFKA_SECURITY_PROTOCOL=PLAINTEXT
+export KAFKA_BOOTSTRAP_SERVERS=localhost:9092
+export LIVEDATA_INSTRUMENT=odin
+
+# Start Kafka broker
+docker-compose -f docker-compose.yml up
+# Init Kafka - only if the `kafka_init` in the docker-compose.yml fails.
+docker exec -it DOCKER_CONTAINER_ID sh /scripts/setup-kafka-topics.sh odin
+
+# Fake Detector Data
+python -m ess.livedata.services.fake_detectors \
+    --instrument ${LIVEDATA_INSTRUMENT:-dummy} \
+    --nexus-file iron_simulation_sample_small.nxs
+
+# Detector Data Service
+python -m ess.livedata.services.detector_data \
+    --instrument ${LIVEDATA_INSTRUMENT:-dummy} \
+    --dev
+
+# Monitor Data Service
+python -m ess.livedata.services.monitor_data \
+    --instrument ${LIVEDATA_INSTRUMENT:-dummy} \
+    --dev
+
+# Dashboard
+python -m pip install -e ".[dashboard]"
+gunicorn ess.livedata.dashboard.reduction_wsgi:application -b 0.0.0.0:5007
